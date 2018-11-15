9d882ac7d215c1cf67671934cefd46811ba4606a
Konstantin <phpconf@gmail.com>
2018-11-15 17:32:23 +0300

    client usage docs

---
+++ pkg/client/doc.go
@@ -0,0 +1,31 @@
+/*
+Package client provides HTTP client for waves nodes.
+
+Creating client with default params:
+
+	c, err := client.NewClient()
+	...
+
+Client can accept custom node url, http client, and api key:
+
+	c, err := client.NewClient(client.Options{
+		Client:  &http.Client{Timeout: 30 * time.Second},
+		BaseUrl: "https://nodes.wavesnodes.com",
+		ApiKey:  "ApiKey",
+	})
+	...
+
+Simple example of client usage:
+
+	c, err := client.NewClient()
+	if err != nil {
+		// handle error
+	}
+	body, response, err := c.Blocks.First(context.Background())
+	if err != nil {
+		// handle error
+	}
+	...
+
+*/
+package client
