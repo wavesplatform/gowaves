c7d6e38ffe5386e2615e154bee4c0a0f6409f9c2
Pavel Dolgov <pdolgov99@gmail.com>
2019-02-05 15:35:35 +0300

    Add first test for account storage

---
+++ pkg/storage/accountsstorage_test.go
@@ -0,0 +1,126 @@
+package storage
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/wavesplatform/gowaves/pkg/crypto"
+	"github.com/wavesplatform/gowaves/pkg/keyvalue"
+	"github.com/wavesplatform/gowaves/pkg/proto"
+)
+
+func createDbDirs() (string, string, string, error) {
+	dbDir0, err := ioutil.TempDir(os.TempDir(), "dbDir0")
+	if err != nil {
+		return "", "", "", errors.Errorf("Failed to create db dir: %v", err)
+	}
+	dbDir1, err := ioutil.TempDir(os.TempDir(), "dbDir1")
+	if err != nil {
+		return "", "", "", errors.Errorf("Failed to create db dir: %v", err)
+	}
+	dbDir2, err := ioutil.TempDir(os.TempDir(), "dbDir2")
+	if err != nil {
+		return "", "", "", errors.Errorf("Failed to create db dir: %v", err)
+	}
+	return dbDir0, dbDir1, dbDir2, nil
+}
+
+func cleanDbDirs(dbDir0, dbDir1, dbDir2 string, t *testing.T) {
+	if err := os.RemoveAll(dbDir0); err != nil {
+		t.Fatalf("Failed to close test data dirs: %v", err)
+	}
+	if err := os.RemoveAll(dbDir1); err != nil {
+		t.Fatalf("Failed to close test data dirs: %v", err)
+	}
+	if err := os.RemoveAll(dbDir2); err != nil {
+		t.Fatalf("Failed to close test data dirs: %v", err)
+	}
+}
+
+func createAccountsStorage(dbDir0, dbDir1, dbDir2 string) (*AccountsStorage, error) {
+	globalStor, err := keyvalue.NewKeyVal(dbDir0, 0)
+	if err != nil {
+		return nil, err
+	}
+	addr2Index, err := keyvalue.NewKeyVal(dbDir1, 0)
+	if err != nil {
+		return nil, err
+	}
+	asset2Index, err := keyvalue.NewKeyVal(dbDir2, 0)
+	if err != nil {
+		return nil, err
+	}
+	return NewAccountsStorage(globalStor, addr2Index, asset2Index, "")
+}
+
+func genAddr(magicNumber int) proto.Address {
+	var addr proto.Address
+	for i := 0; i < proto.AddressSize; i++ {
+		addr[i] = byte(magicNumber)
+	}
+	return addr
+}
+
+func genBlockID(magicNumber int) crypto.Signature {
+	var blockID crypto.Signature
+	for i := 0; i < crypto.SignatureSize; i++ {
+		blockID[i] = byte(magicNumber)
+	}
+	return blockID
+}
+
+func TestBalances(t *testing.T) {
+	dbDir0, dbDir1, dbDir2, err := createDbDirs()
+	if err != nil {
+		t.Fatalf("Can not create database directories: %v\n", err)
+	}
+	stor, err := createAccountsStorage(dbDir0, dbDir1, dbDir2)
+	if err != nil {
+		t.Fatalf("Can not create AccountsStorage: %v\n", err)
+	}
+	// Set first balance.
+	balance := uint64(100)
+	blockID := genBlockID(0)
+	addr := genAddr(1)
+	if err := stor.SetAccountBalance(addr, nil, balance, blockID); err != nil {
+		t.Fatalf("Faied to set account balance:%v\n", err)
+	}
+	newBalance, err := stor.AccountBalance(addr, nil)
+	if err != nil {
+		t.Fatalf("Failed to retrieve account balance: %v\n", err)
+	}
+	if newBalance != balance {
+		t.Errorf("Balances are not equal: %d and %d\n", balance, newBalance)
+	}
+	// Set balance in same block.
+	balance = 2500
+	if err := stor.SetAccountBalance(addr, nil, balance, blockID); err != nil {
+		t.Fatalf("Faied to set account balance:%v\n", err)
+	}
+	newBalance, err = stor.AccountBalance(addr, nil)
+	if err != nil {
+		t.Fatalf("Failed to retrieve account balance: %v\n", err)
+	}
+	if newBalance != balance {
+		t.Errorf("Balances are not equal: %d and %d\n", balance, newBalance)
+	}
+	// Set balance in new block.
+	balance = 10
+	blockID = genBlockID(1)
+	if err := stor.SetAccountBalance(addr, nil, balance, blockID); err != nil {
+		t.Fatalf("Faied to set account balance:%v\n", err)
+	}
+	newBalance, err = stor.AccountBalance(addr, nil)
+	if err != nil {
+		t.Fatalf("Failed to retrieve account balance: %v\n", err)
+	}
+	if newBalance != balance {
+		t.Errorf("Balances are not equal: %d and %d\n", balance, newBalance)
+	}
+	defer cleanDbDirs(dbDir0, dbDir1, dbDir2, t)
+}
+
+func TestRollbackBlock(t *testing.T) {
+}
