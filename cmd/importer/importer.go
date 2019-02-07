9df1b64559f7a60754c0b463cf4a324ef5e51dc2
Pavel Dolgov <pdolgov99@gmail.com>
2019-02-07 23:57:49 +0300

    State: add rollback test on real blocks

--- cmd/importer/importer.go
+++ cmd/importer/importer.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"github.com/wavesplatform/gowaves/pkg/state"
+	"github.com/wavesplatform/gowaves/pkg/storage"
+	"github.com/wavesplatform/gowaves/pkg/util"
 )
 
 var (
@@ -21,10 +23,44 @@ func main() {
 	if len(*blockchainPath) == 0 {
 		log.Fatalf("You must specify blockchain-path option.")
 	}
+	rw, rwPath, err := storage.CreateTestBlockReadWriter(*batchSize, 8, 8)
+	if err != nil {
+		log.Fatalf("CreateTesBlockReadWriter: %v\n", err)
+	}
+	idsFile, err := rw.BlockIdsFilePath()
+	if err != nil {
+		log.Fatalf("Failed to get path of ids file: %v\n", err)
+	}
+	stor, storPath, err := storage.CreateTestAccountsStorage(idsFile)
+	if err != nil {
+		log.Fatalf("CreateTestAccountStorage: %v\n", err)
+	}
+
+	defer func() {
+		if err := rw.Close(); err != nil {
+			log.Fatalf("Failed to close BlockReadWriter: %v\n", err)
+		}
+		if err := util.CleanTemporaryDirs(rwPath); err != nil {
+			log.Fatalf("Failed to clean data dirs: %v\n", err)
+		}
+		if err := util.CleanTemporaryDirs(storPath); err != nil {
+			log.Fatalf("Failed to clean data dirs: %v\n", err)
+		}
+	}()
+
+	manager, err := state.NewStateManager(stor, rw)
+	if err != nil {
+		log.Fatalf("Failed to create state manager: %v.\n", err)
+	}
 	start := time.Now()
-	if err := state.CheckState(*blockchainPath, *balancesPath, *batchSize, *nBlocks); err != nil {
-		log.Fatalf("CheckState(): %v\n", err)
+	if err := state.Apply(*blockchainPath, *nBlocks, manager); err != nil {
+		log.Fatalf("Failed to apply blocks: %v\n", err)
 	}
 	elapsed := time.Since(start)
 	fmt.Printf("Import took %s\n", elapsed)
+	if len(*balancesPath) != 0 {
+		if err := state.CheckBalances(*balancesPath, stor); err != nil {
+			log.Fatalf("CheckBalances(): %v\n", err)
+		}
+	}
 }
--- pkg/state/state_test.go
+++ pkg/state/state_test.go
@@ -6,11 +6,15 @@ import (
 	"testing"
 
 	"github.com/pkg/errors"
+	"github.com/wavesplatform/gowaves/pkg/storage"
+	"github.com/wavesplatform/gowaves/pkg/util"
 )
 
 const (
 	BATCH_SIZE    = 1000
 	BLOCKS_NUMBER = 1000
+	FIRST_HEIGHT  = 900
+	SECOND_HEIGHT = 30
 )
 
 func getLocalDir() (string, error) {
@@ -21,14 +25,62 @@ func getLocalDir() (string, error) {
 	return filepath.Dir(filename), nil
 }
 
-func TestBlockAccept(t *testing.T) {
+func TestBlockAcceptAndRollback(t *testing.T) {
 	dir, err := getLocalDir()
 	if err != nil {
 		t.Fatalf("Failed to get local dir: %v\n", err)
 	}
 	blocksPath := filepath.Join(dir, "..", "storage", "testdata", "blocks-10000")
-	balancesPath := filepath.Join(dir, "testdata", "accounts-1000")
-	if err := CheckState(blocksPath, balancesPath, BATCH_SIZE, BLOCKS_NUMBER); err != nil {
-		t.Fatalf("CheckState: %v\n", err)
+	balancesPath0 := filepath.Join(dir, "testdata", "accounts-1000")
+	balancesPath1 := filepath.Join(dir, "testdata", "accounts-900")
+	balancesPath2 := filepath.Join(dir, "testdata", "accounts-30")
+	rw, rwPath, err := storage.CreateTestBlockReadWriter(BATCH_SIZE, 8, 8)
+	if err != nil {
+		t.Fatalf("CreateTesBlockReadWriter: %v\n", err)
+	}
+	idsFile, err := rw.BlockIdsFilePath()
+	if err != nil {
+		t.Fatalf("Failed to get path of ids file: %v\n", err)
+	}
+	stor, storPath, err := storage.CreateTestAccountsStorage(idsFile)
+	if err != nil {
+		t.Fatalf("CreateTestAccountStorage: %v\n", err)
+	}
+
+	defer func() {
+		if err := rw.Close(); err != nil {
+			t.Fatalf("Failed to close BlockReadWriter: %v\n", err)
+		}
+		if err := util.CleanTemporaryDirs(rwPath); err != nil {
+			t.Fatalf("Failed to clean data dirs: %v\n", err)
+		}
+		if err := util.CleanTemporaryDirs(storPath); err != nil {
+			t.Fatalf("Failed to clean data dirs: %v\n", err)
+		}
+	}()
+
+	manager, err := NewStateManager(stor, rw)
+	if err != nil {
+		t.Fatalf("Failed to create state manager: %v.\n", err)
+	}
+	if err := Apply(blocksPath, BLOCKS_NUMBER, manager); err != nil {
+		t.Fatalf("Failed to import: %v\n", err)
+	}
+	if err := CheckBalances(balancesPath0, stor); err != nil {
+		t.Fatalf("CheckBalances(): %v\n", err)
+	}
+
+	if err := manager.RollbackToHeight(FIRST_HEIGHT); err != nil {
+		t.Fatalf("Rollback(): %v\n", err)
+	}
+	if err := CheckBalances(balancesPath1, stor); err != nil {
+		t.Fatalf("CheckBalances(): %v\n", err)
+	}
+
+	if err := manager.RollbackToHeight(SECOND_HEIGHT); err != nil {
+		t.Fatalf("Rollback(): %v\n", err)
+	}
+	if err := CheckBalances(balancesPath2, stor); err != nil {
+		t.Fatalf("CheckBalances(): %v\n", err)
 	}
 }
