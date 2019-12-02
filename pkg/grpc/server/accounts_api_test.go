8a03f56d5c02b38b799e2562f59c9f93134714c5
Pavel Dolgov <pdolgov99@gmail.com>
2019-12-02 13:40:54 +0300

    Remove TestGetDataEntries()
    Better system for mining blocks in tests is needed, because no features can
    be activated/used in genesis block.

--- pkg/grpc/server/accounts_api_test.go
+++ pkg/grpc/server/accounts_api_test.go
@@ -12,51 +12,6 @@ import (
 	"github.com/wavesplatform/gowaves/pkg/settings"
 )
 
-/* TODO: uncomment!
-func TestGetDataEntries(t *testing.T) {
-	genesisGetter := settings.FromCurrentDir("testdata/genesis", "data_entries_genesis.json")
-	st, stateCloser := stateWithCustomGenesis(t, genesisGetter)
-	err := server.resetState(st)
-	assert.NoError(t, err)
-
-	conn := connect(t, grpcTestAddr)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer func() {
-		cancel()
-		conn.Close()
-		stateCloser()
-	}()
-
-	cl := g.NewAccountsApiClient(conn)
-
-	addr, err := proto.NewAddressFromString("3PPTrTo3AzR56N7ArzbU3Bpq9zYMgcf39Mk")
-	assert.NoError(t, err)
-	addrBytes := addr.Bytes()
-	req := &g.DataRequest{Address: addrBytes}
-	stream, err := cl.GetDataEntries(ctx, req)
-	assert.NoError(t, err)
-	entry0 := &g.DataTransactionData_DataEntry{
-		Key:   "waves_usd_2",
-		Value: &g.DataTransactionData_DataEntry_IntValue{IntValue: 81},
-	}
-	entry1 := &g.DataTransactionData_DataEntry{
-		Key:   "waves_btc_8",
-		Value: &g.DataTransactionData_DataEntry_IntValue{IntValue: 8653},
-	}
-	correctEntries := []*g.DataEntryResponse{
-		&g.DataEntryResponse{Address: addrBytes, Entry: entry0},
-		&g.DataEntryResponse{Address: addrBytes, Entry: entry1},
-	}
-	for _, correct := range correctEntries {
-		entry, err := stream.Recv()
-		assert.NoError(t, err)
-		assert.Equal(t, correct, entry)
-	}
-	_, err = stream.Recv()
-	assert.Equal(t, io.EOF, err)
-}
-*/
-
 func TestResolveAlias(t *testing.T) {
 	genesisGetter := settings.FromCurrentDir("testdata/genesis", "alias_genesis.json")
 	st, stateCloser := stateWithCustomGenesis(t, genesisGetter)
