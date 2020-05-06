e1355c110514e9efd08ce61047a2dfd62e746b2e
Pavel Dolgov <pdolgov99@gmail.com>
2020-05-06 15:59:31 +0300

    Speed up statecmp: download in parallel

--- cmd/statecmp/statecmp.go
+++ cmd/statecmp/statecmp.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -21,10 +22,12 @@ const (
 )
 
 var (
-	logLevel    = flag.String("log-level", "INFO", "Logging level. Supported levels: DEBUG, INFO, WARN, ERROR, FATAL.")
-	nodesStr    = flag.String("nodes", "", "Addresses of nodes; comma separated.")
-	startHeight = flag.Int("start-height", 1, "Start height.")
-	endHeight   = flag.Int("end-height", 2, "End height.")
+	logLevel      = flag.String("log-level", "INFO", "Logging level. Supported levels: DEBUG, INFO, WARN, ERROR, FATAL.")
+	nodesStr      = flag.String("nodes", "", "Addresses of nodes; comma separated.")
+	startHeight   = flag.Int("start-height", 1, "Start height.")
+	endHeight     = flag.Int("end-height", 2, "End height.")
+	goroutinesNum = flag.Int("goroutines-num", 15, "Number of goroutines that will run for downloading state hashes.")
+	tries         = flag.Int("tries-num", 5, "Number of tries to download.")
 )
 
 func checkAndUpdateURL(s string) (string, error) {
@@ -48,16 +51,29 @@ func checkAndUpdateURL(s string) (string, error) {
 }
 
 func loadStateHash(ctx context.Context, cl *client.Client, height uint64) (*proto.StateHash, error) {
-	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
 	defer cancel()
-	sh, _, err := cl.Debug.StateHash(ctx, height)
-	if err != nil {
-		return nil, err
+	var sh *proto.StateHash
+	var err error
+	for i := 0; i < *tries; i++ {
+		sh, _, err = cl.Debug.StateHash(ctx, height)
+		if err == nil {
+			return sh, nil
+		}
 	}
-	return sh, nil
+	return nil, err
 }
 
-func printDifferentResults(res map[proto.FieldsHashes]*nodesGroup) {
+type printer struct {
+	lock sync.Mutex
+}
+
+func (p *printer) printDifferentResults(height uint64, res map[proto.FieldsHashes]*nodesGroup) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	zap.S().Infof("Height: %d; following state hashes are different:", height)
+	zap.S().Info("=============================================================")
 	for fh, nodes := range res {
 		hashJs, err := json.Marshal(fh)
 		if err != nil {
@@ -66,6 +82,7 @@ func printDifferentResults(res map[proto.FieldsHashes]*nodesGroup) {
 		zap.S().Info(string(hashJs))
 		zap.S().Infof("Nodes: %v\n", nodes.nodes)
 	}
+	zap.S().Info("=============================================================")
 }
 
 type stateHashInfo struct {
@@ -92,6 +109,68 @@ func (ng *nodesGroup) addNode(node string) {
 	ng.nodes = append(ng.nodes, node)
 }
 
+func manageHeight(ctx context.Context, height uint64, nodes []string, clients []*client.Client, p *printer) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	results := make(chan hashResult, len(clients))
+	for i, cl := range clients {
+		node := nodes[i]
+		go func(cl *client.Client, node string) {
+			sh, err := loadStateHash(ctx, cl, height)
+			res := hashResult{
+				res: stateHashInfo{
+					hash: sh,
+					node: node,
+				},
+				err: err,
+			}
+			results <- res
+		}(cl, node)
+	}
+	differentResults := make(map[proto.FieldsHashes]*nodesGroup)
+	for range clients {
+		hr := <-results
+		if hr.err != nil {
+			cancel()
+			return hr.err
+		}
+		fh := hr.res.hash.FieldsHashes
+		nodesGroup, ok := differentResults[fh]
+		if !ok {
+			differentResults[fh] = newNodesGroup(hr.res.node)
+		} else {
+			nodesGroup.addNode(hr.res.node)
+		}
+	}
+	if len(differentResults) != 1 {
+		p.printDifferentResults(height, differentResults)
+	}
+	return nil
+}
+
+func download(heightChan chan uint64, errChan chan error, nodes []string, clients []*client.Client) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	p := &printer{}
+	var wg sync.WaitGroup
+	for i := 0; i < *goroutinesNum; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for height := range heightChan {
+				if err := manageHeight(ctx, height, nodes, clients, p); err != nil {
+					cancel()
+					zap.S().Errorf("Failed to load some hashes: %v", err)
+					errChan <- err
+					break
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
 func main() {
 	flag.Parse()
 
@@ -116,51 +195,26 @@ func main() {
 		}
 	}
 
-	results := make(chan hashResult, len(clients))
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	identical := true
+	heightChan := make(chan uint64)
+	errChan := make(chan error, *goroutinesNum)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		download(heightChan, errChan, nodes, clients)
+		wg.Done()
+	}()
 	for h := uint64(*startHeight); h < uint64(*endHeight); h++ {
-		for i, cl := range clients {
-			node := nodes[i]
-			go func(cl *client.Client, node string, h uint64) {
-				sh, err := loadStateHash(ctx, cl, h)
-				res := hashResult{
-					res: stateHashInfo{
-						hash: sh,
-						node: node,
-					},
-					err: err,
-				}
-				results <- res
-			}(cl, node, h)
+		gotErr := false
+		select {
+		case heightChan <- h:
+		case <-errChan:
+			gotErr = true
 		}
-		differentResults := make(map[proto.FieldsHashes]*nodesGroup)
-		for range clients {
-			hr := <-results
-			if hr.err != nil {
-				cancel()
-				zap.S().Fatalf("Failed to load some hash: %v", hr.err)
-			}
-			fh := hr.res.hash.FieldsHashes
-			nodesGroup, ok := differentResults[fh]
-			if !ok {
-				differentResults[fh] = newNodesGroup(hr.res.node)
-			} else {
-				nodesGroup.addNode(hr.res.node)
-			}
-		}
-		if len(differentResults) != 1 {
-			identical = false
-			zap.S().Infof("Height: %d; following state hashes are different:", h)
-			zap.S().Info("=============================================================")
-			printDifferentResults(differentResults)
-			zap.S().Info("=============================================================")
+		if gotErr {
+			break
 		}
 	}
-	if identical {
-		zap.S().Info("All hashes are identical at all heights!")
-	}
-	close(results)
+	close(heightChan)
+	wg.Wait()
+	zap.S().Info("Finished to compare states.")
 }
