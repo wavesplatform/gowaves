717a48da0f8e3d2ad01211126eb4b5c2d322c49b
Alexey Kiselev <alexey.kiselev@gmail.com>
2020-08-05 10:43:21 +0300

    WIP

---
+++ pkg/fride/estimators.go
@@ -0,0 +1,15 @@
+package fride
+
+import "github.com/pkg/errors"
+
+func estimateV1(program *Program) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func estimateV2(program *Program) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+func estimateV3(program *Program) (int, error) {
+	return 0, errors.New("not implemented")
+}
