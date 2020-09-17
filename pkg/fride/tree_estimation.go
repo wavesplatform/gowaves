ad9a868193b97ccaef0c7a25c1d35e1af9cace8b
Alexey Kiselev <alexey.kiselev@gmail.com>
2020-09-17 14:40:43 +0300

    Fully functional EstimatorV2 except stack overflow issue on 3 testnet scripts

--- pkg/fride/tree_estimation.go
+++ pkg/fride/tree_estimation.go
@@ -14,6 +14,16 @@ func EstimateTree(tree *Tree, v int) (int, int, map[string]int, error) {
 			return 0, 0, nil, errors.Wrap(err, "failed to estimate with tree estimator V1")
 		}
 		return max, verifier, functions, nil
+	case 2:
+		te, err := newTreeEstimatorV2(tree)
+		if err != nil {
+			return 0, 0, nil, errors.Wrap(err, "failed to estimate with tree estimator V2")
+		}
+		max, verifier, functions, err := te.estimate()
+		if err != nil {
+			return 0, 0, nil, errors.Wrap(err, "failed to estimate with tree estimator V2")
+		}
+		return max, verifier, functions, nil
 	case 3:
 		te, err := newTreeEstimatorV3(tree)
 		if err != nil {
