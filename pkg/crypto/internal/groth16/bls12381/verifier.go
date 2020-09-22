2baad5d65083cae436034e6bba5d0cb22734fa3f
Alexandr Dolgavin <adolgavin@web3tech.ru>
2020-09-22 17:35:24 +0300

    removed useless alocations

--- pkg/crypto/internal/groth16/bls12381/verifier.go
+++ pkg/crypto/internal/groth16/bls12381/verifier.go
@@ -11,11 +11,12 @@ func ProofVerify(vk *VerificationKey, proof *Proof, inputs []*big.Int) (bool, er
 		return false, errors.New("len(inputs)+1 != len(vk.IC)")
 	}
 	vkX := vk.Ic[0]
+	PointG1 := bls.NewG1()
 	for i := 0; i < len(inputs); i++ {
-		vkX = bls.NewG1().Add(bls.NewG1().Zero(), vkX, bls.NewG1().MulScalar(bls.NewG1().Zero(), vk.Ic[i+1], inputs[i]))
+		vkX = PointG1.Add(PointG1.Zero(), vkX, PointG1.MulScalar(PointG1.Zero(), vk.Ic[i+1], inputs[i]))
 	}
 	// g1 = { -A(G1), alpha(G1), inputs(G1), C(G1) }
-	g1 := []*bls.PointG1{bls.NewG1().Neg(bls.NewG1().Zero(), proof.A), vk.AlphaG1, vkX, proof.C}
+	g1 := []*bls.PointG1{PointG1.Neg(PointG1.Zero(), proof.A), vk.AlphaG1, vkX, proof.C}
 	// g2 = { B(G2), beta(G2), gamma(G2), delta(G2) }
 	g2 := []*bls.PointG2{proof.B, vk.BetaG2, vk.GammaG2, vk.DeltaG2}
 
