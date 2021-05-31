4544777afde1f4a6758545ddade6eff19e0448b8
esuwu <sanya554455@gmail.com>
2021-05-31 15:34:31 +0300

    Added the limit of invocation scripts with different versions

--- pkg/ride/tree_evaluation_test.go
+++ pkg/ride/tree_evaluation_test.go
@@ -3773,7 +3773,6 @@ func TestPaymentsDifferentScriptVersion4(t *testing.T) {
 
 	addressCallable, err = proto.NewAddressFromString("3P8eZVKS7a4troGckytxaefLAi9w7P5aMna")
 	require.NoError(t, err)
-	recipientCallable := proto.NewRecipientFromAddress(addressCallable)
 	addressCallablePK, err = smartStateDappFromDapp().NewestScriptPKByAddr(addressCallable)
 	require.NoError(t, err)
 
@@ -3810,14 +3809,6 @@ func TestPaymentsDifferentScriptVersion4(t *testing.T) {
 
 	id = bytes.Repeat([]byte{0}, 32)
 
-	expectedDataEntryWrites := []*proto.DataEntryScriptAction{
-		{Entry: &proto.IntegerDataEntry{Key: "int", Value: 1}, Sender: &addressCallablePK},
-	}
-
-	expectedTransferWrites := []*proto.TransferScriptAction{
-		{Recipient: recipientCallable, Sender: &addrPK, Amount: 1, Asset: proto.OptionalAsset{}},
-	}
-
 	smartState := smartStateDappFromDapp
 
 	thisAddress = addr
@@ -3840,40 +3831,12 @@ func TestPaymentsDifferentScriptVersion4(t *testing.T) {
 
 	res, err := CallFunction(env, tree, "test", proto.Arguments{})
 
-	require.NoError(t, err)
+	require.Error(t, err)
 	r, ok := res.(DAppResult)
-	require.True(t, ok)
-	require.True(t, r.res)
+	require.False(t, ok)
+	require.False(t, r.res)
 
-	sr, err := proto.NewScriptResult(r.actions, proto.ScriptErrorMessage{})
-	require.NoError(t, err)
-
-	expectedActionsResult := &proto.ScriptResult{
-		DataEntries:  expectedDataEntryWrites,
-		Transfers:    expectedTransferWrites,
-		Issues:       make([]*proto.IssueScriptAction, 0),
-		Reissues:     make([]*proto.ReissueScriptAction, 0),
-		Burns:        make([]*proto.BurnScriptAction, 0),
-		Sponsorships: make([]*proto.SponsorshipScriptAction, 0),
-		Leases:       make([]*proto.LeaseScriptAction, 0),
-		LeaseCancels: make([]*proto.LeaseCancelScriptAction, 0),
-	}
-
-	assert.Equal(t, expectedActionsResult, sr)
-
-	expectedDiffResult := initWrappedState(smartState(), env).diff
-
-	balanceMain := diffBalance{asset: proto.OptionalAsset{}, regular: 9999}
-	balanceSender := diffBalance{asset: proto.OptionalAsset{}, regular: 0}
-	balanceCallable := diffBalance{asset: proto.OptionalAsset{}, regular: 1}
-	expectedDiffResult.balances[addr.String()+proto.OptionalAsset{}.String()] = balanceMain
-	expectedDiffResult.balances[senderAddress.String()+proto.OptionalAsset{}.String()] = balanceSender
-	expectedDiffResult.balances[addressCallable.String()+proto.OptionalAsset{}.String()] = balanceCallable
-	intEntry := proto.IntegerDataEntry{Key: "int", Value: 1}
-	expectedDiffResult.dataEntries.diffInteger["int"+addressCallable.String()] = intEntry
-
-	assert.Equal(t, expectedDiffResult.dataEntries, wrappedSt.diff.dataEntries)
-	assert.Equal(t, expectedDiffResult.balances, wrappedSt.diff.balances)
+	tearDownDappFromDapp()
 
 	tearDownDappFromDapp()
 
@@ -3934,7 +3897,6 @@ func TestPaymentsDifferentScriptVersion3(t *testing.T) {
 
 	addressCallable, err = proto.NewAddressFromString("3P8eZVKS7a4troGckytxaefLAi9w7P5aMna")
 	require.NoError(t, err)
-	recipientCallable := proto.NewRecipientFromAddress(addressCallable)
 	addressCallablePK, err = smartStateDappFromDapp().NewestScriptPKByAddr(addressCallable)
 	require.NoError(t, err)
 
@@ -3971,14 +3933,6 @@ func TestPaymentsDifferentScriptVersion3(t *testing.T) {
 
 	id = bytes.Repeat([]byte{0}, 32)
 
-	expectedDataEntryWrites := []*proto.DataEntryScriptAction{
-		{Entry: &proto.IntegerDataEntry{Key: "int", Value: 1}, Sender: &addressCallablePK},
-	}
-
-	expectedTransferWrites := []*proto.TransferScriptAction{
-		{Recipient: recipientCallable, Sender: &addrPK, Amount: 1, Asset: proto.OptionalAsset{}},
-	}
-
 	smartState := smartStateDappFromDapp
 
 	thisAddress = addr
@@ -4001,40 +3955,10 @@ func TestPaymentsDifferentScriptVersion3(t *testing.T) {
 
 	res, err := CallFunction(env, tree, "test", proto.Arguments{})
 
-	require.NoError(t, err)
+	require.Error(t, err)
 	r, ok := res.(DAppResult)
-	require.True(t, ok)
-	require.True(t, r.res)
-
-	sr, err := proto.NewScriptResult(r.actions, proto.ScriptErrorMessage{})
-	require.NoError(t, err)
-
-	expectedActionsResult := &proto.ScriptResult{
-		DataEntries:  expectedDataEntryWrites,
-		Transfers:    expectedTransferWrites,
-		Issues:       make([]*proto.IssueScriptAction, 0),
-		Reissues:     make([]*proto.ReissueScriptAction, 0),
-		Burns:        make([]*proto.BurnScriptAction, 0),
-		Sponsorships: make([]*proto.SponsorshipScriptAction, 0),
-		Leases:       make([]*proto.LeaseScriptAction, 0),
-		LeaseCancels: make([]*proto.LeaseCancelScriptAction, 0),
-	}
-
-	assert.Equal(t, expectedActionsResult, sr)
-
-	expectedDiffResult := initWrappedState(smartState(), env).diff
-
-	balanceMain := diffBalance{asset: proto.OptionalAsset{}, regular: 9999}
-	balanceSender := diffBalance{asset: proto.OptionalAsset{}, regular: 0}
-	balanceCallable := diffBalance{asset: proto.OptionalAsset{}, regular: 1}
-	expectedDiffResult.balances[addr.String()+proto.OptionalAsset{}.String()] = balanceMain
-	expectedDiffResult.balances[senderAddress.String()+proto.OptionalAsset{}.String()] = balanceSender
-	expectedDiffResult.balances[addressCallable.String()+proto.OptionalAsset{}.String()] = balanceCallable
-	intEntry := proto.IntegerDataEntry{Key: "int", Value: 1}
-	expectedDiffResult.dataEntries.diffInteger["int"+addressCallable.String()] = intEntry
-
-	assert.Equal(t, expectedDiffResult.dataEntries, wrappedSt.diff.dataEntries)
-	assert.Equal(t, expectedDiffResult.balances, wrappedSt.diff.balances)
+	require.False(t, ok)
+	require.False(t, r.res)
 
 	tearDownDappFromDapp()
 
