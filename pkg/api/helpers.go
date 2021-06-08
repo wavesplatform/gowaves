b260a24f3f9f16686cb6a13c3cb457a7f0e78d97
Nikolay Eskov <mr.eskov1@yandex.ru>
2021-06-08 06:31:01 +0300

    Created new node HTTP API route: GET /node/status.

--- pkg/api/helpers.go
+++ pkg/api/helpers.go
@@ -5,3 +5,9 @@ import "time"
 func unixMillis(t time.Time) int64 {
 	return t.UnixNano() / 1_000_000
 }
+
+func fromUnixMillis(timestampMillis int64) time.Time {
+	sec := timestampMillis / 1_000
+	nsec := (timestampMillis % 1_000) * 1_000_000
+	return time.Unix(sec, nsec)
+}
