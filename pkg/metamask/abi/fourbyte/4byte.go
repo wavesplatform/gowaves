ddcba90d142dd0e6a6cf4da4e407cd6a343d682e
Nikolay Eskov <mr.eskov1@yandex.ru>
2021-07-27 04:59:47 +0300

    Add function BuildSignatureFromRideFunctionMeta which allows to build ABI function signature from ride function metadata.

--- pkg/metamask/abi/fourbyte/4byte.go
+++ pkg/metamask/abi/fourbyte/4byte.go
@@ -1,10 +1,13 @@
 package fourbyte
 
 import (
+	"bytes"
+	"encoding"
 	"encoding/hex"
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/wavesplatform/gowaves/pkg/metamask"
+	"github.com/wavesplatform/gowaves/pkg/ride/meta"
 	"strings"
 )
 
@@ -29,6 +32,21 @@ func NewSignature(funcName string, inputArgs Arguments) Signature {
 	}
 	sig := fmt.Sprintf("%s(%s)", funcName, strings.Join(typeStrings, ","))
 	return Signature(sig)
+}
+
+func BuildSignatureFromRideFunctionMeta(fn meta.Function, addPayments bool) (Signature, error) {
+	builder := functionTextBuilder{
+		addPayments:  addPayments,
+		functionMeta: fn,
+	}
+	signature, err := builder.MarshalText()
+	if err != nil {
+		return "", errors.Errorf("failed to build signature for function %s with %d arguments",
+			fn.Name,
+			len(fn.Arguments),
+		)
+	}
+	return Signature(signature), nil
 }
 
 func (s Signature) String() string {
@@ -64,6 +82,272 @@ func (s *Selector) FromHex(hexSelector string) error {
 		return errors.Errorf("invalid hex selector bytes, expected %d, received %d", len(s), len(bts))
 	}
 	copy(s[:], bts)
+	return nil
+}
+
+func rideMetaTypeToTextMarshaler(metaT meta.Type) (encoding.TextMarshaler, error) {
+	switch t := metaT.(type) {
+	case meta.SimpleType:
+		switch t {
+		case meta.Int:
+			// nickeskov: it's RideInt type
+			marshaler := intTextBuilder{
+				size:     64,
+				unsigned: false,
+			}
+			return marshaler, nil
+		case meta.Bytes:
+			return bytesTextBuilder{}, nil
+		case meta.Boolean:
+			return booleanTextBuilder{}, nil
+		case meta.String:
+			return stringTextBuilder{}, nil
+		default:
+			return nil, errors.Errorf("invalid ride simple type (%d)", t)
+		}
+	case meta.ListType:
+		inner, err := rideMetaTypeToTextMarshaler(t.Inner)
+		if err != nil {
+			return nil, errors.Wrapf(err,
+				"failed to create text marshaler for list type, inner type %T", t.Inner,
+			)
+		}
+		return sliceTextBuilder{t: inner}, nil
+	case meta.UnionType:
+		marshalers := make([]encoding.TextMarshaler, 0, len(t))
+		for _, unionUnitT := range t {
+			unionUnitMarshaler, err := rideMetaTypeToTextMarshaler(unionUnitT)
+			if err != nil {
+				return nil, errors.Wrapf(err,
+					"failed to create text marshaler for union type, inner type %T", unionUnitT,
+				)
+			}
+			marshalers = append(marshalers, unionUnitMarshaler)
+		}
+		return unionTextBuilder(marshalers), nil
+	default:
+		return nil, errors.Errorf("unsupported ride metadata type, type %T", t)
+	}
+}
+
+type intTextBuilder struct {
+	size     int
+	unsigned bool
+}
+
+func (itb intTextBuilder) MarshalText() (text []byte, err error) {
+	if itb.size%8 != 0 || itb.size <= 0 || itb.size > 256 {
+		return nil, errors.Errorf("invalid int type size (%d)", itb.size)
+	}
+	unsignedPrefix := ""
+	if itb.unsigned {
+		unsignedPrefix = "u"
+	}
+	return []byte(fmt.Sprintf("%sint%d", unsignedPrefix, itb.size)), nil
+}
+
+type bytesTextBuilder struct{}
+
+func (btb bytesTextBuilder) MarshalText() (text []byte, err error) {
+	return []byte("bytes"), nil
+}
+
+type booleanTextBuilder struct{}
+
+func (btb booleanTextBuilder) MarshalText() (text []byte, err error) {
+	return []byte("bool"), nil
+}
+
+type stringTextBuilder struct{}
+
+func (stb stringTextBuilder) MarshalText() (text []byte, err error) {
+	return []byte("string"), nil
+}
+
+type sliceTextBuilder struct {
+	t encoding.TextMarshaler
+}
+
+func (stb sliceTextBuilder) MarshalText() (text []byte, err error) {
+	marshaled, err := stb.t.MarshalText()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal %T", stb.t)
+	}
+	return []byte(fmt.Sprintf("%s[]", marshaled)), nil
+}
+
+type tupleTextBuilder []encoding.TextMarshaler
+
+func (ttb tupleTextBuilder) MarshalText() (text []byte, err error) {
+	if len(ttb) == 0 {
+		return []byte("()"), nil
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString("(")
+	for i := 0; i < len(ttb)-1; i++ {
+		elem := ttb[i]
+		marshaled, err := elem.MarshalText()
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to marshal tuple element, type %T", elem)
+		}
+		// nickeskov: can't fail
+		_, _ = fmt.Fprintf(&buf, "%s,", marshaled)
+	}
+	// nickeskov: marshal the last one
+	elem := ttb[len(ttb)-1]
+	marshaled, err := elem.MarshalText()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal %T", elem)
+	}
+	_, _ = fmt.Fprintf(&buf, "%s)", marshaled)
+	return buf.Bytes(), nil
+
+	//elements := make([]string, 0, len(ttb))
+	//for _, elem := range ttb {
+	//	marshaled, err := elem.MarshalText()
+	//	if err != nil {
+	//		return nil, errors.Wrapf(err, "failed to marshal tuple element, type %T", elem)
+	//	}
+	//	elements = append(elements, string(marshaled))
+	//}
+	//
+	//return []byte(fmt.Sprintf("(%s)", strings.Join(elements, ","))), nil
+
+}
+
+type unionTextBuilder []encoding.TextMarshaler
+
+func (utb unionTextBuilder) MarshalText() (text []byte, err error) {
+	if len(utb) == 0 {
+		return nil, errors.Errorf("can't marshal union with no elements")
+	}
+	unionElements := make([]encoding.TextMarshaler, len(utb)+1)
+	// nickeskov: create index element to represent ride tuple in ethereum abi
+	unionElements[0] = intTextBuilder{
+		size:     8,
+		unsigned: true,
+	}
+	copy(unionElements[1:], utb)
+	text, err = tupleTextBuilder(unionElements).MarshalText()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal union")
+	}
+	return text, nil
+}
+
+type addressTextBuilder struct{}
+
+func (atb addressTextBuilder) MarshalText() (text []byte, err error) {
+	return []byte("address"), nil
+}
+
+type paymentTextBuilder struct{}
+
+func (ptb paymentTextBuilder) MarshalText() (text []byte, err error) {
+	tupleBuilder := tupleTextBuilder{
+		addressTextBuilder{},
+		// nickeskov: asset amount field in payment
+		intTextBuilder{
+			size:     64,
+			unsigned: true,
+		},
+	}
+	text, err = tupleBuilder.MarshalText()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal payment")
+	}
+	return text, nil
+}
+
+type functionTextBuilder struct {
+	addPayments  bool
+	functionMeta meta.Function
+}
+
+func (ftb functionTextBuilder) MarshalText() (text []byte, err error) {
+	var buf bytes.Buffer
+	// nickeksov: can't fail
+	_, _ = fmt.Fprintf(&buf, "%s(", ftb.functionMeta.Name)
+	if len(ftb.functionMeta.Arguments) == 0 {
+		if err := ftb.handlePayments(&buf); err != nil {
+			return nil, errors.Wrap(err, "failed to handle payments in functionTextBuilder")
+		}
+		buf.WriteString(")")
+		return buf.Bytes(), nil
+	}
+
+	for i := 0; i < len(ftb.functionMeta.Arguments)-1; i++ {
+		arg := ftb.functionMeta.Arguments[i]
+		marshaler, err := rideMetaTypeToTextMarshaler(arg)
+		if err != nil {
+			return nil, errors.Errorf("failed to create function argument text marshaler, type %T", arg)
+		}
+		marshaled, err := marshaler.MarshalText()
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to marshal function argument, type %T", arg)
+		}
+		// nickeskov: can't fail
+		_, _ = fmt.Fprintf(&buf, "%s,", marshaled)
+	}
+	// nickeskov: marshal the last one
+	arg := ftb.functionMeta.Arguments[len(ftb.functionMeta.Arguments)-1]
+	marshaler, err := rideMetaTypeToTextMarshaler(arg)
+	if err != nil {
+		return nil, errors.Errorf("failed to create function argument text marshaler, type %T", arg)
+	}
+	marshaled, err := marshaler.MarshalText()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal function argument, type %T", arg)
+	}
+	// nickeskov: can't fail
+	_, _ = fmt.Fprintf(&buf, "%s", marshaled)
+
+	if ftb.addPayments {
+		buf.WriteString(",")
+		if err := ftb.handlePayments(&buf); err != nil {
+			return nil, errors.Wrap(err, "failed to handle payments in functionTextBuilder")
+		}
+	}
+	buf.WriteString(")")
+	return buf.Bytes(), nil
+
+	//sliceLen := len(ftb.functionMeta.Arguments)
+	//if ftb.addPayments {
+	//	sliceLen += 1
+	//}
+	//elements := make([]string, 0, sliceLen)
+	//for _, arg := range ftb.functionMeta.Arguments {
+	//	marshaler, err := rideMetaTypeToTextMarshaler(arg)
+	//	if err != nil {
+	//		return nil, errors.Errorf("failed to create function argument text marshaler, type %T", arg)
+	//	}
+	//	marshaled, err := marshaler.MarshalText()
+	//	if err != nil {
+	//		return nil, errors.Wrapf(err, "failed to marshal function argument, type %T", arg)
+	//	}
+	//	elements = append(elements, string(marshaled))
+	//}
+	//if ftb.addPayments {
+	//	payments, err := sliceTextBuilder{t: paymentTextBuilder{}}.MarshalText()
+	//	if err != nil {
+	//		return nil, errors.Wrap(err, "failed to marshal payments")
+	//	}
+	//	elements = append(elements, string(payments))
+	//}
+	//
+	//return []byte(fmt.Sprintf("%s(%s)", ftb.functionMeta.Name, strings.Join(elements, ","))), nil
+
+}
+
+func (ftb *functionTextBuilder) handlePayments(buf *bytes.Buffer) error {
+	if ftb.addPayments {
+		payments, err := sliceTextBuilder{t: paymentTextBuilder{}}.MarshalText()
+		if err != nil {
+			return errors.Wrap(err, "failed to marshal payments")
+		}
+		_, _ = fmt.Fprintf(buf, "%s", payments)
+	}
 	return nil
 }
 
--- pkg/metamask/abi/fourbyte/fourbyte_test.go
+++ pkg/metamask/abi/fourbyte/fourbyte_test.go
@@ -2,6 +2,7 @@ package fourbyte
 
 import (
 	"github.com/stretchr/testify/require"
+	"github.com/wavesplatform/gowaves/pkg/ride/meta"
 	"testing"
 )
 
@@ -10,4 +11,40 @@ func TestSignature_Selector(t *testing.T) {
 
 	const testSignatureMint = Signature("mint(string,string,address,uint256,uint256,uint256,uint256)")
 	require.Equal(t, "bdc01110", testSignatureMint.Selector().Hex())
+}
+
+func TestBuildSignatureFromRideFunctionMeta(t *testing.T) {
+	testdata := []struct {
+		expectedSig Signature
+		metadata    meta.Function
+		payments    bool
+	}{
+		{expectedSig: "meta()", metadata: meta.Function{Name: "meta"}, payments: false},
+		{
+			expectedSig: "hardMeta(int64,string,bytes,bool,bool[],(uint8,bool,string,bytes,int64)[])",
+			metadata: meta.Function{
+				Name: "hardMeta",
+				Arguments: []meta.Type{
+					meta.Int,
+					meta.String,
+					meta.Bytes,
+					meta.Boolean,
+					meta.ListType{Inner: meta.Boolean},
+					meta.ListType{Inner: meta.UnionType{meta.Boolean, meta.String, meta.Bytes, meta.Int}},
+				},
+			},
+			payments: false,
+		},
+		{
+			expectedSig: "metaPayments(bool,bytes,(address,uint64)[])",
+			metadata:    meta.Function{Name: "metaPayments", Arguments: []meta.Type{meta.Boolean, meta.Bytes}},
+			payments:    true,
+		},
+	}
+
+	for _, test := range testdata {
+		actualSig, err := BuildSignatureFromRideFunctionMeta(test.metadata, test.payments)
+		require.NoError(t, err)
+		require.Equal(t, test.expectedSig, actualSig)
+	}
 }
