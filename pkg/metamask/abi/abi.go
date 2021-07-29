9635051faac97b8c84605571b167d8b43b8fde63
esuwu <sanya554455@gmail.com>
2021-07-29 11:15:40 +0300

    Added types representation for abi json

--- pkg/metamask/abi/abi.go
+++ pkg/metamask/abi/abi.go
@@ -3,17 +3,9 @@ package abi
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/wavesplatform/gowaves/pkg/metamask/abi/fourbyte"
 )
-
-func parseNew(data []byte) (*fourbyte.DecodedCallData, error) {
-	db, err := fourbyte.NewDatabase()
-	if err != nil {
-		fmt.Println(err)
-	}
-	decodedData, err := db.ParseCallDataNew(data)
-	return decodedData, err
-}
 
 func parseRide(data []byte) (*fourbyte.DecodedCallData, error) {
 	db, err := fourbyte.NewDatabase()
@@ -43,8 +35,31 @@ func getJsonAbi(metaDApp map[fourbyte.Selector]fourbyte.Method) ([]byte, error) 
 		arguments := make([]Arg, 0)
 		for _, arg := range method.Inputs {
 			a := Arg{Name: arg.Name, Type: arg.Type.String()}
+
+			switch arg.Type.T {
+			case fourbyte.TupleTy:
+				a.Type = "tuple[]"
+			case fourbyte.SliceTy:
+				a.Type = fmt.Sprintf(arg.Type.String(), "[]")
+			case fourbyte.StringTy: // variable arrays are written at the end of the return bytes
+				a.Type = "string"
+			case fourbyte.IntTy:
+				a.Type = "int64"
+			case fourbyte.UintTy:
+				a.Type = "uint256"
+			case fourbyte.BoolTy:
+				a.Type = "bool"
+			case fourbyte.AddressTy:
+				a.Type = "address"
+			case fourbyte.BytesTy:
+				a.Type = "bytes"
+			default:
+				return nil, errors.Errorf("abi: unknown type %s", arg.Type.String())
+			}
+
 			arguments = append(arguments, a)
 		}
+		// payments
 		arguments = append(arguments, Arg{Name: "", Type: "tuple[]", Components: []Arg{{Name: "", Type: "address"}, {Name: "", Type: "uint256"}}})
 		m := ABI{Name: method.RawName, Type: "function", Inputs: arguments}
 		abi = append(abi, m)
--- pkg/metamask/abi/fourbyte/selector.go
+++ pkg/metamask/abi/fourbyte/selector.go
@@ -63,32 +63,6 @@ func (db *Database) MethodBySelector(id Selector) (Method, error) {
 	return Method{}, fmt.Errorf("signature %v not found", id.String())
 }
 
-func (db *Database) ParseCallDataNew(data []byte) (*DecodedCallData, error) {
-	// If the data is empty, we have a plain value transfer, nothing more to do
-	if len(data) == 0 {
-		return nil, errors.New("transaction doesn't contain data")
-	}
-	// Validate the call data that it has the 4byte prefix and the rest divisible by 32 bytes
-	if len(data) < 4 {
-		return nil, errors.New("transaction data is not valid ABI: missing the 4 byte call prefix")
-	}
-	if n := len(data) - 4; n%32 != 0 {
-		return nil, errors.Errorf("transaction data is not valid ABI (length should be a multiple of 32 (was %d))", n)
-	}
-	var selector Selector
-	copy(selector[:], data[:len(selector)])
-	method, err := db.MethodBySelector(selector)
-	if err != nil {
-		return nil, errors.Errorf("Transaction contains data, but the ABI signature could not be found: %v", err)
-	}
-
-	info, err := parseArgData(&method, data[len(selector):])
-	if err != nil {
-		return nil, errors.Errorf("Transaction contains data, but provided ABI signature could not be verified: %v", err)
-	}
-	return info, nil
-}
-
 func (db *Database) ParseCallDataRide(data []byte) (*DecodedCallData, error) {
 	// If the data is empty, we have a plain value transfer, nothing more to do
 	if len(data) == 0 {
@@ -137,26 +111,6 @@ func (da *DecodedArg) DecodedValue() interface{} {
 
 func (da *DecodedArg) InternalType() byte {
 	return byte(da.Soltype.Type.T)
-}
-
-func parseArgData(method *Method, argData []byte) (*DecodedCallData, error) {
-	values, paymentsABI, err := method.Inputs.UnpackValues(argData)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to unpack Inputs arguments ABI data")
-	}
-	payments, err := unpackPayments(paymentsABI)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to unpack payments")
-	}
-
-	decoded := DecodedCallData{Signature: method.Sig.String(), Name: method.RawName, Payments: payments}
-	for i := 0; i < len(method.Inputs); i++ {
-		decoded.Inputs = append(decoded.Inputs, DecodedArg{
-			Soltype: method.Inputs[i],
-			Value:   values[i],
-		})
-	}
-	return &decoded, nil
 }
 
 func parseArgDataToRideTypes(method *Method, argData []byte) (*DecodedCallData, error) {
