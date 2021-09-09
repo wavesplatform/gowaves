f4fbcd4179f193a29e723a305ac0837a67795038
esuwu <sanya554455@gmail.com>
2021-09-10 01:41:56 +0300

    Added signatures for tests

--- pkg/state/ethereum_tx_test.go
+++ pkg/state/ethereum_tx_test.go
@@ -26,6 +26,38 @@ func defaultTxAppender(t *testing.T) txAppender {
 	return txAppender
 }
 
+func defaultEthereumLegacyTxData(value int64, to *proto.EthereumAddress) *proto.EthereumLegacyTx {
+	return &proto.EthereumLegacyTx{
+		Value:    big.NewInt(value),
+		To:       to,
+		Data:     nil,
+		GasPrice: big.NewInt(1),
+		Nonce:    2,
+		Gas:      100,
+	}
+}
+
+func defaultEthereumDynamicFeeTx(value int64, to *proto.EthereumAddress) *proto.EthereumDynamicFeeTx {
+	return &proto.EthereumDynamicFeeTx{
+		Value:    big.NewInt(value),
+		To:       to,
+		Data:     nil,
+		Nonce:    2,
+		Gas:      100,
+	}
+}
+
+func defaultEthereumAccessListTx(value int64, to *proto.EthereumAddress) *proto.EthereumAccessListTx {
+	return &proto.EthereumAccessListTx{
+		Value:    big.NewInt(value),
+		To:       to,
+		Data:     nil,
+		GasPrice: big.NewInt(1),
+		Nonce:    2,
+		Gas:      100,
+	}
+}
+
 func TestEthereumTransferWaves(t *testing.T) {
 	appendTxParams := defaultAppendTxParams()
 	txAppender := defaultTxAppender(t)
@@ -36,37 +68,41 @@ func TestEthereumTransferWaves(t *testing.T) {
 	recipientBytes, err := base58.Decode("a783d1CBABe28d25E64aDf84477C4687c1411f94") // 0x241Cf7eaf669E0d2FDe4Ba3a534c20B433F4c43d
 	assert.NoError(t, err)
 	recipientEth := proto.BytesToEthereumAddress(recipientBytes)
-	ethereumTxData := &proto.EthereumLegacyTx{
-		Value:    big.NewInt(1000000000000000),
-		To:       &recipientEth,
-		Data:     nil,
-		GasPrice: big.NewInt(1),
-		Nonce:    2,
-		Gas:      100,
+
+	var TxSeveralData []proto.EthereumTxData
+	TxSeveralData = append(TxSeveralData, defaultEthereumLegacyTxData(1000000000000000, &recipientEth), defaultEthereumDynamicFeeTx(1000000000000000, &recipientEth), defaultEthereumAccessListTx(1000000000000000, &recipientEth))
+
+
+	for _, txData := range TxSeveralData {
+
+		tx := proto.EthereumTransaction{
+			Inner:    txData,
+			TxKind:   &proto.EthereumTransferWavesTx{},
+			ID:       nil,
+			SenderPK: senderPK,
+		}
+
+		applRes, err := txAppender.handleDefaultTransaction(&tx, appendTxParams, false)
+		assert.NoError(t, err)
+		assert.True(t, applRes.status)
+
+		sender, err := tx.SenderPK.EthereumAddress().ToWavesAddress(0)
+		assert.NoError(t, err)
+
+		wavesAsset := proto.NewOptionalAssetWaves()
+
+		senderKey := byteKey(sender, wavesAsset.ToID())
+
+		recipient, err := recipientEth.ToWavesAddress(0)
+		assert.NoError(t, err)
+		recipientKey := byteKey(recipient, wavesAsset.ToID())
+
+		senderBalance := applRes.changes.diff[string(senderKey)].balance
+		recipientBalance := applRes.changes.diff[string(recipientKey)].balance
+
+		assert.Equal(t, senderBalance, int64(-100100))
+		assert.Equal(t, recipientBalance, int64(100000))
 	}
-	tx := proto.EthereumTransaction{
-		Inner:    ethereumTxData,
-		TxKind:   &proto.EthereumTransferWavesTx{},
-		ID:       nil,
-		SenderPK: senderPK,
-	}
-
-	applRes, err := txAppender.handleDefaultTransaction(&tx, appendTxParams, false)
-	assert.NoError(t, err)
-	assert.True(t, applRes.status)
-	sender, err := tx.SenderPK.EthereumAddress().ToWavesAddress(0)
-	assert.NoError(t, err)
-	wavesAsset := proto.NewOptionalAssetWaves()
-
-	senderKey := byteKey(sender, wavesAsset.ToID())
-
-	recipient, err := recipientEth.ToWavesAddress(0)
-	assert.NoError(t, err)
-	recipientKey := byteKey(recipient, wavesAsset.ToID())
-
-	senderBalance := applRes.changes.diff[string(senderKey)].balance
-	recipientBalance := applRes.changes.diff[string(recipientKey)].balance
-
-	assert.Equal(t, senderBalance, int64(-100100))
-	assert.Equal(t, recipientBalance, int64(100000))
 }
+
+
