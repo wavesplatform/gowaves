cc08d5d9377f8b9a70307e5aca8038ee3606d4e0
Alexey Kiselev <alexey.kiselev@gmail.com>
2021-12-03 11:43:04 +0300

    Fixed issue with incorrect keys during batched storage normalization. Fixed incorrect initial value for storage keys enumeration. Storage version incresed. Tests on history records sizes added.

--- pkg/state/constants.go
+++ pkg/state/constants.go
@@ -25,7 +25,7 @@ const (
 
 	// StateVersion is current version of state internal storage formats.
 	// It increases when backward compatibility with previous storage version is lost.
-	StateVersion = 10
+	StateVersion = 11
 
 	// Memory limit for address transactions. flush() is called when this
 	// limit is exceeded.
