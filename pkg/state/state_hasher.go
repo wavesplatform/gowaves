2d28bdd82357a2520632fec197b5bacf31a86829
Alexey Kiselev <alexey.kiselev@gmail.com>
2022-04-04 18:47:13 +0400

    Explicit state hash data initialization added

--- pkg/state/state_hasher.go
+++ pkg/state/state_hasher.go
@@ -34,7 +34,8 @@ type stateForHashes struct {
 
 func newStateForHashes() *stateForHashes {
 	return &stateForHashes{
-		pos: make(map[string]int),
+		pos:  make(map[string]int),
+		data: make(stateComponents, 0),
 	}
 }
 
@@ -49,7 +50,7 @@ func (s *stateForHashes) set(key string, c stateComponent) {
 }
 
 func (s *stateForHashes) reset() {
-	s.data = nil
+	s.data = make(stateComponents, 0)
 	s.pos = make(map[string]int)
 }
 
