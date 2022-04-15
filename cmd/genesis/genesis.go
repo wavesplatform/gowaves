63de78d6aac8cc634d02b24288f62907779d6bef
Alexey Kiselev <alexey.kiselev@gmail.com>
2022-04-15 12:59:00 +0400

    Config generation moved to genesis utility. Separate utility removed.

--- cmd/genesis/genesis.go
+++ cmd/genesis/genesis.go
@@ -12,9 +12,11 @@ import (
 	"time"
 	"unicode"
 
+	"github.com/mr-tron/base58"
 	"github.com/pkg/errors"
 	"github.com/wavesplatform/gowaves/pkg/crypto"
 	"github.com/wavesplatform/gowaves/pkg/proto"
+	"github.com/wavesplatform/gowaves/pkg/settings"
 	"github.com/wavesplatform/gowaves/pkg/util/genesis_generator"
 )
 
@@ -33,6 +35,9 @@ func run() error {
 		pairs      string
 		baseTarget uint64
 		timestamp  int64
+		signature  string
+		config     bool
+		shift      time.Duration
 	)
 	flag.StringVar(&scheme, "scheme", "C", "Network scheme byte, defaults to 'C'")
 	flag.StringVar(&seed, "seed", "", "Master seed as Base58 string")
@@ -40,6 +45,10 @@ func run() error {
 	flag.StringVar(&pairs, "pairs", "", "Comma separated pairs of address or account seed and amount to produce genesis transactions, eg '3MvRmBpZf6Cm14dY5Nrrq2pj4587EzGTnj4:100_000_000,8GVECo9addsbFumLsmnAU3Cfz7UiF5TGm64zkZnfntdA:100_000'")
 	flag.Uint64Var(&baseTarget, "base-target", 0, "Base Target value")
 	flag.Int64Var(&timestamp, "timestamp", time.Now().UnixMilli(), "Block and transactions timestamp in ms, defaults to current time")
+	flag.StringVar(&signature, "signature", "", "Genesis block signature as Base58 string")
+	flag.BoolVar(&config, "config", false, "Generate configuration")
+	flag.DurationVar(&shift, "time-shift", 0, "Genesis block and transactions timestamp time shift in text format (eg: +1h, -2h3s)")
+
 	flag.Parse()
 
 	var (
@@ -56,6 +65,9 @@ func run() error {
 		return errors.Errorf("ivalid timestamp '%d'", timestamp)
 	}
 	ts = uint64(timestamp)
+	if shift != 0 {
+		ts = uint64(time.Now().Add(shift).UnixMilli())
+	}
 	switch {
 	case len(pairs) != 0 && len(seed) == 0 && len(amounts) == 0:
 		txs, err := parsePairs(pairs, sc, ts)
@@ -68,7 +80,7 @@ func run() error {
 		if err != nil {
 			return err
 		}
-		txs, err := makeTransactions([]byte(seed), as, sc, ts)
+		txs, err := makeTransactions(parseSeed(seed), as, sc, ts)
 		if err != nil {
 			return err
 		}
@@ -81,14 +93,49 @@ func run() error {
 	}
 	bt = baseTarget
 
-	block, err := genesis_generator.GenerateGenesisBlock(sc, transactions, bt, ts)
-	if err != nil {
-		return err
+	var block *proto.Block
+	if signature != "" {
+		sig, err := crypto.NewSignatureFromBase58(signature)
+		if err != nil {
+			return errors.Wrapf(err, "invalid signature '%s'", signature)
+		}
+		b, err := genesis_generator.RecreateGenesisBlock(sc, transactions, bt, ts, sig)
+		if err != nil {
+			return errors.Wrap(err, "failed to recreate genesis block")
+		}
+		block = b
+	} else {
+		b, err := genesis_generator.GenerateGenesisBlock(sc, transactions, bt, ts)
+		if err != nil {
+			return errors.Wrapf(err, "failed to generate genesis block")
+		}
+		ok, err := b.VerifySignature(sc)
+		if err != nil {
+			return errors.Wrapf(err, "failed to validate genesis block signature")
+		}
+		if !ok {
+			return errors.Errorf("genesis block signature '%s' is not valid", signature)
+		}
+		block = b
 	}
-	js, err := json.Marshal(block)
-	if err != nil {
-		return err
+	var js []byte
+	if config {
+		cfg := *settings.DefaultCustomSettings
+		cfg.Genesis = *block
+		cfg.AddressSchemeCharacter = sc
+		var err error
+		js, err = json.Marshal(cfg)
+		if err != nil {
+			return err
+		}
+	} else {
+		var err error
+		js, err = json.Marshal(block)
+		if err != nil {
+			return err
+		}
 	}
+	fmt.Println()
 	fmt.Println(string(js))
 	return nil
 }
@@ -148,7 +195,7 @@ func makeTransactions(seed []byte, amounts []uint64, scheme byte, ts uint64) ([]
 	r := make([]genesis_generator.GenesisTransactionInfo, 0, len(amounts))
 	for i, amount := range amounts {
 		iv := make([]byte, 4)
-		binary.BigEndian.PutUint32(iv, uint32(i))
+		binary.LittleEndian.PutUint32(iv, uint32(i))
 		s := append(iv, seed...)
 		h, err := crypto.SecureHash(s)
 		if err != nil {
@@ -165,4 +212,12 @@ func makeTransactions(seed []byte, amounts []uint64, scheme byte, ts uint64) ([]
 		r = append(r, genesis_generator.GenesisTransactionInfo{Address: addr, Amount: amount, Timestamp: ts})
 	}
 	return r, nil
+}
+
+func parseSeed(s string) []byte {
+	r, err := base58.Decode(s)
+	if err != nil {
+		return []byte(s)
+	}
+	return r
 }
