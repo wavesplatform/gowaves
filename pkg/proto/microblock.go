4d18aa5122bfe1c2de24229dbdf3c3165fb0a991
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-05-12 20:38:40 +0300

    Small fix in 'pkg/proto/microblock.go'.

--- pkg/proto/microblock.go
+++ pkg/proto/microblock.go
@@ -223,9 +223,9 @@ func (a *MicroBlockMessage) WriteTo(w io.Writer) (int64, error) {
 	buf := bytebufferpool.Get()
 	defer bytebufferpool.Put(buf)
 
-	n, err := buf.Write(a.Body)
+	_, err := buf.Write(a.Body)
 	if err != nil {
-		return int64(n), err
+		return 0, err
 	}
 
 	h, err := MakeHeader(ContentIDMicroblock, buf.Bytes())
