7048e938c6c81924131fa9a3eb72994ebc4d3e1d
Alexey Kiselev <alexey.kiselev@gmail.com>
2022-06-02 13:17:36 +0400

    Check on in-memory mapping of account on it's number added. Check on … (#755)
    * Check on in-memory mapping of account on it's number added. Check on iterator removed from function as unnecessary. Function renamed to newwesEnrtyExists.
    
    * WIP
    
    * Test on rollback fixed, resets of correct test object added.
    
    * Comment added

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -1153,7 +1153,7 @@ func (s *stateManager) needToCancelLeases(blockchainHeight uint64) (bool, error)
 		// Only needed for MainNet.
 		return s.settings.Type == settings.MainNet, nil
 	case rideV5Height:
-		// Cancellation of leasings to stolen aliases only required for MainNet
+		// Cancellation of leases to stolen aliases only required for MainNet
 		return s.settings.Type == settings.MainNet, nil
 	default:
 		return false, nil
@@ -1387,7 +1387,7 @@ func (s *stateManager) addBlocks(initialisation bool) (*proto.Block, error) {
 	if err := s.stor.handleStateHashes(height, ids, initialisation); err != nil {
 		return nil, wrapErr(ModificationError, err)
 	}
-	// Validate consensus (i.e. that all of the new blocks were mined fairly).
+	// Validate consensus (i.e. that all the new blocks were mined fairly).
 	if err := s.cv.ValidateHeadersBatch(headers[:pos], height); err != nil {
 		return nil, wrapErr(ValidationError, err)
 	}
@@ -1731,7 +1731,7 @@ func (s *stateManager) IsStateUntouched(account proto.Recipient) (bool, error) {
 	if err != nil {
 		return false, wrapErr(RetrievalError, err)
 	}
-	entryExist, err := s.stor.accountsDataStor.entryExists(addr, s.filter)
+	entryExist, err := s.stor.accountsDataStor.newestEntryExists(addr, s.filter)
 	if err != nil {
 		return false, wrapErr(RetrievalError, err)
 	}
