b217364fbf02f8c3d47a1fb64cc7d4ffb9c88fe7
Artem Starshov <artstarshov@list.ru>
2022-06-22 11:48:12 +0300

    fix tests

--- pkg/node/peer_manager/active_peers_test.go
+++ pkg/node/peer_manager/active_peers_test.go
@@ -11,7 +11,7 @@ import (
 func TestBasic(t *testing.T) {
 	active := NewActivePeers()
 
-	info, ok := active.GetPeerWithMaxScore()
+	_, ok := active.GetPeerWithMaxScore()
 	assert.False(t, ok)
 
 	peer1 := &mock.Peer{
@@ -25,16 +25,18 @@ func TestBasic(t *testing.T) {
 	active.Add(peer1)
 	active.Add(peer2)
 
-	info, ok = active.GetPeerWithMaxScore()
+	info, ok := active.GetPeerWithMaxScore()
 	assert.True(t, ok)
 	assert.Equal(t, peer1, info.peer)
 
-	active.UpdateScore(peer2.ID(), big.NewInt(100))
+	err := active.UpdateScore(peer2.ID(), big.NewInt(100))
+	assert.NoError(t, err)
 	info, ok = active.GetPeerWithMaxScore()
 	assert.True(t, ok)
 	assert.Equal(t, peer2, info.peer)
 
-	active.UpdateScore(peer1.ID(), big.NewInt(100))
+	err = active.UpdateScore(peer1.ID(), big.NewInt(100))
+	assert.NoError(t, err)
 	info, ok = active.GetPeerWithMaxScore()
 	assert.True(t, ok)
 	assert.Equal(t, peer2, info.peer)
--- pkg/node/peer_manager/peer_storage_test.go
+++ pkg/node/peer_manager/peer_storage_test.go
@@ -1,12 +1,14 @@
 package peer_manager
 
 import (
+	"testing"
+	"time"
+
 	"github.com/golang/mock/gomock"
 	"github.com/wavesplatform/gowaves/pkg/mock"
 	"github.com/wavesplatform/gowaves/pkg/node/peer_manager/storage"
+	"github.com/wavesplatform/gowaves/pkg/p2p/peer"
 	"github.com/wavesplatform/gowaves/pkg/proto"
-	"testing"
-	"time"
 )
 
 func TestPeerManagerImpl_Suspend(t *testing.T) {
@@ -19,9 +21,10 @@ func TestPeerManagerImpl_Suspend(t *testing.T) {
 
 	p := mock.NewMockPeer(ctrl)
 	gomock.InOrder(
+		p.EXPECT().ID().Return(peer.PeerID("some-id")),
 		p.EXPECT().Close(),
 		p.EXPECT().RemoteAddr().Return(tcpAddr),
-		p.EXPECT().ID().Return("some-id"),
+		p.EXPECT().ID().Return(peer.PeerID("some-id")),
 	)
 
 	peerStorage := mock.NewMockPeerStorage(ctrl)
