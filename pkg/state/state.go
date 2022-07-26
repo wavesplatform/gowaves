6729b0292c4ea12738bae0e34e8267835a359a20
Alexey Kiselev <alexey.kiselev@gmail.com>
2022-07-26 18:04:05 +0400

    Code blocks reorganization

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -367,10 +367,26 @@ func newStateManager(dataDir string, amend bool, params StateParams, settings *s
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create new state instance")
 	}
-	state, err = handleGenesisBlock(state, dataDir, params, settings, amend)
+	height, err := state.Height()
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to handle genesis block")
+		return nil, err
 	}
+	// 0 state height means that no blocks are found in state, so blockchain history is empty
+	if height == 0 {
+		// We have to close incoming state, add the Genesis block and then reopen state again
+		if err := state.Close(); err != nil {
+			return nil, errors.Wrap(err, "failed to close state before genesis block initialization")
+		}
+		if err := initializeStageWithGenesisBlock(dataDir, params, settings); err != nil {
+			return nil, err
+		}
+		// Set `state` to newly opened instance
+		state, err = newStateManagerInternal(dataDir, amend, params, settings)
+		if err != nil {
+			return nil, err
+		}
+	}
+	state.setGenesisBlock(&settings.Genesis)
 	if err := state.loadLastBlock(); err != nil {
 		return nil, wrapErr(RetrievalError, err)
 	}
@@ -477,45 +493,35 @@ func newStateManagerInternal(dataDir string, amend bool, params StateParams, set
 	return state, nil
 }
 
-func handleGenesisBlock(state *stateManager, path string, params StateParams, settings *settings.BlockchainSettings, amend bool) (*stateManager, error) {
-	height, err := state.Height()
+func initializeStageWithGenesisBlock(path string, params StateParams, settings *settings.BlockchainSettings) (err error) {
+	// The `amend` argument is always false for genesis block because block addition performs only if state height is 0
+	tmp, err := newStateManagerInternal(path, false, params, settings)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// 0 state height means that no blocks are found in state, so blockchain history is empty
-	if height == 0 {
-		// We have to close incoming state, add the Genesis block and then reopen state again
-		if err := state.Close(); err != nil {
-			return nil, errors.Wrap(err, "failed to close state before genesis block initialization")
+	defer func() {
+		if closeErr := tmp.Close(); closeErr != nil {
+			zap.S().Errorf("Failed to close temporary state after genesis block initialization: %v", err)
+			if err != nil {
+				err = errors.Wrapf(err, "and failed to close temporary storage after genesis block initialization: %v", closeErr)
+			} else {
+				err = closeErr
+			}
 		}
-		// The `amend` argument is always false for genesis block because block addition performs only if state height is 0
-		tmp, err := newStateManagerInternal(path, false, params, settings)
-		if err != nil {
-			return nil, err
-		}
-		tmp.setGenesisBlock(&settings.Genesis)
-		// Assign unique block number for this block ID, add this number to the list of valid blocks
-		if err := tmp.stateDB.addBlock(settings.Genesis.BlockID()); err != nil {
-			return nil, err
-		}
-		if err := tmp.addGenesisBlock(); err != nil {
-			return nil, errors.Wrap(err, "failed to apply/save genesis")
-		}
-		// We apply pre-activated features after genesis block, so they aren't active in genesis itself
-		if err := tmp.applyPreActivatedFeatures(state.settings.PreactivatedFeatures, settings.Genesis.BlockID()); err != nil {
-			return nil, errors.Wrap(err, "failed to apply pre-activated features")
-		}
-		if err := tmp.Close(); err != nil {
-			return nil, errors.Wrap(err, "failed to close temporary storage after genesis block initialization")
-		}
-		// Set `state` to newly opened instance
-		state, err = newStateManagerInternal(path, amend, params, settings)
-		if err != nil {
-			return nil, err
-		}
+	}()
+	tmp.setGenesisBlock(&settings.Genesis)
+	// Assign unique block number for this block ID, add this number to the list of valid blocks
+	if err := tmp.stateDB.addBlock(settings.Genesis.BlockID()); err != nil {
+		return err
 	}
-	state.setGenesisBlock(&settings.Genesis)
-	return state, nil
+	if err := tmp.addGenesisBlock(); err != nil {
+		return errors.Wrap(err, "failed to apply/save genesis")
+	}
+	// We apply pre-activated features after genesis block, so they aren't active in genesis itself
+	if err := tmp.applyPreActivatedFeatures(tmp.settings.PreactivatedFeatures, settings.Genesis.BlockID()); err != nil {
+		return errors.Wrap(err, "failed to apply pre-activated features")
+	}
+	return nil
 }
 
 func (s *stateManager) NewestScriptByAccount(account proto.Recipient) (*ast.Tree, error) {
