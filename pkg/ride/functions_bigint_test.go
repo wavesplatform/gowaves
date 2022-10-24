bf20fa14b8b14299fecb02b48fd187754f459748
Alexey Kiselev <alexey.kiselev@gmail.com>
2022-10-24 18:07:54 +0400

    Fixed BitInt Modulo function

--- pkg/ride/functions_bigint_test.go
+++ pkg/ride/functions_bigint_test.go
@@ -7,6 +7,7 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/wavesplatform/gowaves/pkg/crypto"
 	rideMath "github.com/wavesplatform/gowaves/pkg/ride/math"
 
 	"github.com/stretchr/testify/assert"
@@ -250,10 +251,11 @@ func TestModuloBigInt(t *testing.T) {
 		r    rideType
 	}{
 		{[]rideType{toRideBigInt(10), toRideBigInt(6)}, false, toRideBigInt(4)},
-		{[]rideType{toRideBigInt(-10), toRideBigInt(6)}, false, toRideBigInt(2)},
-		{[]rideType{toRideBigInt(10), toRideBigInt(-6)}, false, toRideBigInt(-2)},
+		{[]rideType{toRideBigInt(-10), toRideBigInt(6)}, false, toRideBigInt(-4)},
+		{[]rideType{toRideBigInt(10), toRideBigInt(-6)}, false, toRideBigInt(4)},
 		{[]rideType{toRideBigInt(-10), toRideBigInt(-6)}, false, toRideBigInt(-4)},
 		{[]rideType{toRideBigInt(2), toRideBigInt(2)}, false, toRideBigInt(0)},
+		{[]rideType{rideBigInt{v: decode2CBigInt(crypto.MustBytesFromBase58("A98D6oABd9yshGm29dpxXzSeMi1LhCBSPeGxm7MHVB1c"))}, toRideBigInt(330)}, false, toRideBigInt(-243)},
 		{[]rideType{toRideBigInt(10), toRideBigInt(0)}, true, nil},
 		{[]rideType{toRideBigInt(1), rideUnit{}}, true, nil},
 		{[]rideType{toRideBigInt(1), rideString("x")}, true, nil},
