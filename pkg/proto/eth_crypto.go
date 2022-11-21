f3f2e0ef659e0a7be46c5165e73cc18a06737219
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-11-21 13:45:26 +0300

    Refactor eth crypto (#944)
    * Add test for 'EthereumOrderV4' with leading zeroes in sender public key.
    
    * Refactor 'recoverEthereumPubKey' function.
    
    * Refactor 'pkg/proto/eth_crypto.go'.
    
    * Remove 'TODO' in 'EthereumOrderV4.Verify'.
    
    * Refactor 'pkg/proto/eth_signer.go'.
    
    * Fixed naming in 'NewEthereumSignatureFromVRS' function.
    
    * Fixed naming in 'pkg/proto/eth_signer.go'.
    
    * Fixed naming in 'unmarshalSignatureValuesFastRLP' function.
    
    * Fixed naming in 'EthereumDynamicFeeTx.unmarshalFromFastRLP'.
    
    * Fixed naming in 'EthereumLegacyTx.unmarshalFromFastRLP'.
    
    * Fixed naming in 'EthereumAccessListTx.unmarshalFromFastRLP'.

--- pkg/proto/eth_crypto.go
+++ pkg/proto/eth_crypto.go
@@ -16,13 +16,13 @@ var (
 
 // ValidateEthereumSignatureValues verifies whether the signature values are valid with
 // the given chain rules. The v value is assumed to be either 0 or 1.
-func ValidateEthereumSignatureValues(v byte, r, s *big.Int, homestead bool) bool {
+func ValidateEthereumSignatureValues(v byte, r, s *big.Int) bool {
 	if r.Cmp(big1) < 0 || s.Cmp(big1) < 0 {
 		return false
 	}
 	// reject upper range of s values (ECDSA malleability)
 	// see discussion in secp256k1/libsecp256k1/include/secp256k1.h
-	if homestead && s.Cmp(secp256k1halfN) > 0 {
+	if s.Cmp(secp256k1halfN) > 0 {
 		return false
 	}
 	// Frontier: allow s to be in full N range
@@ -50,6 +50,19 @@ func VerifyEthereumSignature(pubKey *EthereumPublicKey, rBig, sBig *big.Int, has
 // EthereumSignature represents ethereum signature (v, r, s signature values).
 type EthereumSignature struct {
 	sig [ethereumSignatureLength]byte
+}
+
+func NewEthereumSignatureFromVRS(v byte, r, s *big.Int) (EthereumSignature, error) {
+	if !ValidateEthereumSignatureValues(v, r, s) {
+		return EthereumSignature{}, ErrInvalidSig
+	}
+	var sig [ethereumSignatureLength]byte
+	// encode the signature in uncompressed format
+	rBytes, sBytes := r.Bytes(), s.Bytes()
+	copy(sig[32-len(rBytes):32], rBytes)
+	copy(sig[64-len(sBytes):64], sBytes)
+	sig[64] = v
+	return EthereumSignature{sig: sig}, nil
 }
 
 func NewEthereumSignatureFromHexString(hexString string) (ethSig EthereumSignature, err error) {
@@ -98,10 +111,9 @@ func (es *EthereumSignature) UnmarshalBinary(data []byte) error {
 	return nil
 }
 
-func (es EthereumSignature) MarshalJSON() ([]byte, error) {
-	// nickeskov: can't fail
-	data, _ := es.MarshalBinary()
-	return HexBytes(data).MarshalJSON()
+func (es *EthereumSignature) MarshalJSON() ([]byte, error) {
+	sig := es.Bytes()
+	return HexBytes(sig).MarshalJSON()
 }
 
 func (es *EthereumSignature) UnmarshalJSON(bytes []byte) error {
--- pkg/proto/types.go
+++ pkg/proto/types.go
@@ -1869,7 +1869,8 @@ func (o *EthereumOrderV4) Verify(scheme Scheme) (bool, error) {
 	if err != nil {
 		return false, errors.Wrap(err, "failed to validate ethereum signature for EthereumOrderV4")
 	}
-	// TODO(nickeskov): Should we validate 'V' signature value?
+	// we don't have to validate V param here because in ethereum it's used to chainID verification (mostly),
+	// but we have own scheme validation in ethereumTypedDataHash
 	_, r, s := o.Eip712Signature.AsVRS()
 	return VerifyEthereumSignature(o.SenderPK.inner, r, s, hash[:]), nil
 }
