9f0378d904018b808541aa71ff915ae0e7f45b08
irina-pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2022-12-08 16:42:29 +0300

    Alias tests.

---
+++ itests/testdata/alias_tx_testdata.go
@@ -0,0 +1,318 @@
+package testdata
+
+import (
+	"math/rand"
+
+	"github.com/wavesplatform/gowaves/itests/config"
+	f "github.com/wavesplatform/gowaves/itests/fixtures"
+	utl "github.com/wavesplatform/gowaves/itests/utilities"
+	"github.com/wavesplatform/gowaves/pkg/proto"
+)
+
+const (
+	AliasSymbolSet        = "abcdefghijklmnopqrstuvwxyz0123456789.-_@"
+	AliasInvalidSymbolSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ~!|#$%^&*()+=;:/?><|][{}\\\""
+)
+
+type AliasTestData[T any] struct {
+	Account   config.AccountInfo
+	Alias     string
+	Fee       uint64
+	Timestamp uint64
+	ChainID   proto.Scheme
+	Expected  T
+}
+
+type AliasExpectedValuesPositive struct {
+	ExpectedAddress  proto.WavesAddress
+	WavesDiffBalance int64
+	_                struct{}
+}
+
+func (a AliasExpectedValuesPositive) Positive() bool {
+	return true
+}
+
+type AliasExpectedValuesNegative struct {
+	ErrGoMsg          string
+	ErrScalaMsg       string
+	ErrBrdCstGoMsg    string
+	ErrBrdCstScalaMsg string
+	WavesDiffBalance  int64
+	_                 struct{}
+}
+
+type SameAliasExpectedValuesNegative struct {
+	ErrGoMsg                     string
+	ErrScalaMsg                  string
+	ErrBrdCstGoMsg               string
+	ErrBrdCstScalaMsg            string
+	WavesDiffBalanceAfterFirstTx int64
+	ExpectedAddressAfterFirstTx  proto.WavesAddress
+	WavesDiffBalance             int64
+	_                            struct{}
+}
+
+func (a AliasExpectedValuesNegative) Positive() bool {
+	return false
+}
+
+func NewAliasTestData[T any](account config.AccountInfo, alias string, fee uint64, timestamp uint64,
+	chainID proto.Scheme, expected T) *AliasTestData[T] {
+	return &AliasTestData[T]{
+		Account:   account,
+		Alias:     alias,
+		Fee:       fee,
+		Timestamp: timestamp,
+		ChainID:   chainID,
+		Expected:  expected,
+	}
+}
+
+func GetAliasPositiveDataMatrix(suite *f.BaseSuite) map[string]AliasTestData[AliasExpectedValuesPositive] {
+	var t = map[string]AliasTestData[AliasExpectedValuesPositive]{
+		"Valid alias 4 bytes": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(4, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesPositive{
+				ExpectedAddress:  utl.GetAccount(suite, 2).Address,
+				WavesDiffBalance: 100000,
+			}),
+		"Valid alias 15 bytes, middle values for fee": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			100000000000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesPositive{
+				ExpectedAddress:  utl.GetAccount(suite, 2).Address,
+				WavesDiffBalance: 100000000000,
+			}),
+	}
+	return t
+}
+
+func GetAliasMaxPositiveDataMatrix(suite *f.BaseSuite, accNumber int) map[string]AliasTestData[AliasExpectedValuesPositive] {
+	var t = map[string]AliasTestData[AliasExpectedValuesPositive]{
+		"Valid alias 30 bytes, max available fee": *NewAliasTestData(
+			utl.GetAccount(suite, accNumber),
+			utl.RandStringBytes(30, AliasSymbolSet),
+			uint64(utl.GetAvailableBalanceInWavesGo(suite, utl.GetAccount(suite, accNumber).Address)),
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesPositive{
+				ExpectedAddress:  utl.GetAccount(suite, accNumber).Address,
+				WavesDiffBalance: utl.GetAvailableBalanceInWavesGo(suite, utl.GetAccount(suite, accNumber).Address),
+			}),
+	}
+	return t
+}
+
+func GetAliasNegativeDataMatrix(suite *f.BaseSuite) map[string]AliasTestData[AliasExpectedValuesNegative] {
+	var t = map[string]AliasTestData[AliasExpectedValuesNegative]{
+		"Invalid alias, 3 bytes": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(3, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "length should be between 4 and 30",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid alias, invalid symbols": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(4, AliasInvalidSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "Alias should contain only following characters: -.0123456789@_abcdefghijklmnopqrstuvwxyz",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid alias, 31 bytes": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(31, AliasInvalidSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "length should be between 4 and 30",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid alias, empty string": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			"",
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "length should be between 4 and 30",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid alias, invalid encoding": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			"\\u0061\\u0073\\u0073\\u0065",
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "Alias should contain only following characters: -.0123456789@_abcdefghijklmnopqrstuvwxyz",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid fee (fee=0)": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			0,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "insufficient fee",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid fee (0 < fee < min)": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			10,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "(10 in WAVES) does not exceed minimal value of 100000 WAVES",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid fee (fee > max)": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			9223372036854775808,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "failed to parse json message",
+				WavesDiffBalance:  0,
+			}),
+		"Custom chainID": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			'T',
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "State check failed",
+				WavesDiffBalance:  0,
+			}),
+		"Invalid chainID (value=0)": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			0,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "State check failed",
+				WavesDiffBalance:  0,
+			}),
+		"Timestamp more than 7200000ms in the past relative to previous block timestamp": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs()-7215000,
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "is more than 7200000ms in the past relative to previous block timestamp",
+				WavesDiffBalance:  0,
+			}),
+		"Timestamp more than 5400000ms in the future relative to previous block timestamp": *NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs()+54160000,
+			TestChainID,
+			AliasExpectedValuesNegative{
+				ErrGoMsg:          errMsg,
+				ErrScalaMsg:       errMsg,
+				ErrBrdCstGoMsg:    errBrdCstMsg,
+				ErrBrdCstScalaMsg: "is more than 5400000ms in the future relative to block timestamp",
+				WavesDiffBalance:  0,
+			}),
+	}
+	return t
+}
+
+func GetSameAliasNegativeDataMatrix(suite *f.BaseSuite) []AliasTestData[SameAliasExpectedValuesNegative] {
+	var t = []AliasTestData[SameAliasExpectedValuesNegative]{
+		*NewAliasTestData(
+			utl.GetAccount(suite, 2),
+			utl.RandStringBytes(15, AliasSymbolSet),
+			100000,
+			utl.GetCurrentTimestampInMs(),
+			TestChainID,
+			SameAliasExpectedValuesNegative{
+				ErrGoMsg:                     errMsg,
+				ErrScalaMsg:                  errMsg,
+				ErrBrdCstGoMsg:               errBrdCstMsg,
+				ErrBrdCstScalaMsg:            "is already in the state",
+				WavesDiffBalanceAfterFirstTx: 100000,
+				ExpectedAddressAfterFirstTx:  utl.GetAccount(suite, 2).Address,
+				WavesDiffBalance:             0,
+			}),
+	}
+	return t
+}
+
+func GetSameAliasDiffAddressNegativeDataMatrix(suite *f.BaseSuite) []AliasTestData[SameAliasExpectedValuesNegative] {
+	alias := utl.RandStringBytes(15, AliasSymbolSet)
+	accCount := 2
+	var t []AliasTestData[SameAliasExpectedValuesNegative]
+	for i := 0; i < accCount; i++ {
+		t = append(t, *NewAliasTestData(
+			utl.GetAccount(suite, i+2),
+			alias,
+			100000,
+			utl.GetCurrentTimestampInMs()+uint64(rand.Intn(10)),
+			TestChainID,
+			SameAliasExpectedValuesNegative{
+				ErrGoMsg:                     errMsg,
+				ErrScalaMsg:                  errMsg,
+				ErrBrdCstGoMsg:               errBrdCstMsg,
+				ErrBrdCstScalaMsg:            "Alias already claimed",
+				WavesDiffBalanceAfterFirstTx: 100000,
+				WavesDiffBalance:             0,
+			}))
+	}
+	return t
+}
---
+++ itests/utilities/alias_utilities/alias_tx_utility.go
@@ -0,0 +1,63 @@
+package alias_utilities
+
+import (
+	"time"
+
+	"github.com/stretchr/testify/require"
+	f "github.com/wavesplatform/gowaves/itests/fixtures"
+	"github.com/wavesplatform/gowaves/itests/testdata"
+	utl "github.com/wavesplatform/gowaves/itests/utilities"
+	"github.com/wavesplatform/gowaves/pkg/proto"
+)
+
+type MadeTx[T any] func(suite *f.BaseSuite, testdata testdata.AliasTestData[T], version byte, timeout time.Duration, positive bool) utl.ConsideredTransaction
+
+// MakeTxAndGetDiffBalances This function returns txID with init balance before tx and difference balance after tx for both nodes
+func MakeTxAndGetDiffBalances[T any](suite *f.BaseSuite, testdata testdata.AliasTestData[T], version byte,
+	timeout time.Duration, positive bool, madeTx MadeTx[T]) (utl.ConsideredTransaction, utl.BalanceInWaves, utl.BalanceInWaves) {
+	initBalanceGo, initBalanceScala := utl.GetAvailableBalanceInWaves(suite, testdata.Account.Address)
+	tx := madeTx(suite, testdata, version, timeout, positive)
+	actualDiffBalanceInWavesGo, actualDiffBalanceInWavesScala := utl.GetActualDiffBalanceInWaves(
+		suite, testdata.Account.Address, initBalanceGo, initBalanceScala)
+	return *utl.NewConsideredTransaction(tx.TxID, tx.Resp.ResponseGo, tx.Resp.ResponseScala, tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala,
+			tx.BrdCstErr.ErrorBrdCstGo, tx.BrdCstErr.ErrorBrdCstScala),
+		*utl.NewBalanceInWaves(initBalanceGo, initBalanceScala),
+		*utl.NewBalanceInWaves(actualDiffBalanceInWavesGo, actualDiffBalanceInWavesScala)
+}
+
+func NewSignAliasTransaction[T any](suite *f.BaseSuite, version byte, testdata testdata.AliasTestData[T]) proto.Transaction {
+	var tx proto.Transaction
+	alias := proto.NewAlias(testdata.ChainID, testdata.Alias)
+	if version == 1 {
+		tx = proto.NewUnsignedCreateAliasWithSig(
+			testdata.Account.PublicKey, *alias,
+			testdata.Fee, testdata.Timestamp)
+	} else {
+		tx = proto.NewUnsignedCreateAliasWithProofs(version, testdata.Account.PublicKey, *alias,
+			testdata.Fee, testdata.Timestamp)
+	}
+	err := tx.Sign(testdata.ChainID, testdata.Account.SecretKey)
+	suite.T().Logf("Alias Transaction JSON: %s", utl.GetTransactionJsonOrErrMsg(tx))
+	require.NoError(suite.T(), err)
+	return tx
+}
+
+func AliasSend[T any](suite *f.BaseSuite, testdata testdata.AliasTestData[T], version byte, timeout time.Duration, positive bool) utl.ConsideredTransaction {
+	tx := NewSignAliasTransaction(suite, version, testdata)
+	return utl.SendAndWaitTransaction(suite, tx, testdata.ChainID, timeout, positive)
+}
+
+func AliasBroadcast[T any](suite *f.BaseSuite, testdata testdata.AliasTestData[T], version byte, timeout time.Duration, positive bool) utl.ConsideredTransaction {
+	tx := NewSignAliasTransaction(suite, version, testdata)
+	return utl.BroadcastAndWaitTransaction(suite, tx, testdata.ChainID, timeout, positive)
+}
+
+func SendAliasTxAndGetWavesBalances[T any](suite *f.BaseSuite, testdata testdata.AliasTestData[T], version byte, timeout time.Duration, positive bool) (
+	utl.ConsideredTransaction, utl.BalanceInWaves, utl.BalanceInWaves) {
+	return MakeTxAndGetDiffBalances(suite, testdata, version, timeout, positive, AliasSend[T])
+}
+
+func BroadcastAliasTxAndGetWavesBalances[T any](suite *f.BaseSuite, testdata testdata.AliasTestData[T], version byte, timeout time.Duration, positive bool) (
+	utl.ConsideredTransaction, utl.BalanceInWaves, utl.BalanceInWaves) {
+	return MakeTxAndGetDiffBalances(suite, testdata, version, timeout, positive, AliasBroadcast[T])
+}
