31d84673f84f5b0a9a0e052e70206d1370129da4
Irina Pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2022-12-09 13:30:32 +0300

    Alias tx test suite (#927)
    * Add new alias tx tests.
    
    * Add new alias tx api tests.
    
    * Add new alias tx and test data.
    
    * Add new alias tx api test and test data.
    
    * Fix test data.
    
    * Add constraint for 'AliasTestData' generic type.
    
    * New test for same alias.
    
    * New test for same alias.
    
    * Fix tests.
    
    * Small fixes.
    
    * Small fixes.
    
    * CI fix.
    
    * Small fixes.
    
    * Uncommented 'ResolveAlias' checks.
    
    * Fix Test_AliasTxApiMaxValuesPositive.
    
    * Fix Test_SameAliasDiffAddressNegative.
    
    * Fix tests and data.
    
    * Small tests refactoring.
    
    * Tests refactoring.
    
    * Integration tests refactoring.
    
    * Fix sending/broadcasting to nodes.
    
    * Fix marshaling transactions.
    
    * Temp changes in issue tests.
    
    * Fix SendMessage method
    
    * Fix tx versions.
    
    * Fix api issue tx tests.
    
    * Temp fixes.
    
    * Alias tx test suite add account creation helper (#956)
    
    * Added utility functions to create new acctounts and transfer waves funds between accounts.
    Added checks to GetAccount utitlity function.
    
    * Fixed tests AliasMaxValuesPositive и SameAliasDiffAddressesNegative.
    
    * Integration tests chain ID constant made public.
    
    * Fix tests
    
    * Assertion of no errors changed to requirement.
    
    * Fix API tests for alias tx.
    
    * Spliting into positive and negative suites
    
    * Rename file.
    
    * Add ClearBlackList method.
    
    * Previous test files were returned.
    
    * Updating code.
    
    * Fix common symbol set.
    
    * Alias tests.
    
    * Integration tests CI timeout increased to 30m.
    
    Co-authored-by: Nikolay Eskov <mr.eskov1@yandex.ru>
    Co-authored-by: Alexey Kiselev <alexey.kiselev@gmail.com>

--- itests/utilities/common_tx_utility.go
+++ itests/utilities/common_tx_utility.go
@@ -2,7 +2,11 @@ package utilities
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"math/rand"
+	"strconv"
 	"testing"
 	"time"
 
@@ -15,42 +19,126 @@ import (
 )
 
 const (
-	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!|#$%^&*()_+=\\\";:/?><|][{}"
+	CommonSymbolSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!|#$%^&*()_+=\\\";:/?><|][{}"
 )
 
-type BroadcastedTransaction struct {
-	TxID             crypto.Digest
-	ResponseGo       *client.Response
+type Response struct {
+	ResponseGo    *client.Response
+	ResponseScala *client.Response
+}
+
+type BalanceInWaves struct {
+	BalanceInWavesGo    int64
+	BalanceInWavesScala int64
+}
+
+type BalanceInAsset struct {
+	BalanceInAssetGo    int64
+	BalanceInAssetScala int64
+}
+
+type WaitingError struct {
+	ErrWtGo    error
+	ErrWtScala error
+}
+
+type BroadcastingError struct {
 	ErrorBrdCstGo    error
-	ResponseScala    *client.Response
 	ErrorBrdCstScala error
 }
 
-func NewBroadcastedTransaction(txId crypto.Digest, responseGo *client.Response, errBrdCstGo error,
-	responseScala *client.Response, errBrdCstScala error) *BroadcastedTransaction {
-	return &BroadcastedTransaction{
-		TxID:             txId,
-		ResponseGo:       responseGo,
-		ErrorBrdCstGo:    errBrdCstGo,
-		ResponseScala:    responseScala,
-		ErrorBrdCstScala: errBrdCstScala,
+type ConsideredTransaction struct {
+	TxID      crypto.Digest
+	WtErr     WaitingError
+	Resp      Response
+	BrdCstErr BroadcastingError
+}
+
+func NewBalanceInWaves(balanceGo, balanceScala int64) *BalanceInWaves {
+	return &BalanceInWaves{
+		BalanceInWavesGo:    balanceGo,
+		BalanceInWavesScala: balanceScala,
 	}
 }
 
-func RandStringBytes(n int) string {
+func NewConsideredTransaction(txId crypto.Digest, respGo, respScala *client.Response,
+	errWtGo, errWtScala, errBrdCstGo, errBrdCstScala error) *ConsideredTransaction {
+	return &ConsideredTransaction{
+		TxID: txId,
+		Resp: Response{
+			ResponseGo:    respGo,
+			ResponseScala: respScala,
+		},
+		WtErr: WaitingError{
+			ErrWtGo:    errWtGo,
+			ErrWtScala: errWtScala,
+		},
+		BrdCstErr: BroadcastingError{
+			ErrorBrdCstGo:    errBrdCstGo,
+			ErrorBrdCstScala: errBrdCstScala,
+		},
+	}
+}
+
+func RandStringBytes(n int, symbolSet string) string {
 	b := make([]byte, n)
 	for j := range b {
-		b[j] = letterBytes[rand.Intn(len(letterBytes))]
+		b[j] = symbolSet[rand.Intn(len(symbolSet))]
 	}
 	return string(b)
+}
+
+func GetTransactionJsonOrErrMsg(tx proto.Transaction) string {
+	var result string
+	jsonStr, err := json.Marshal(tx)
+	if err != nil {
+		result = fmt.Sprintf("Failed to create tx JSON: %s", err)
+	} else {
+		result = string(jsonStr)
+	}
+	return result
 }
 
 func GetCurrentTimestampInMs() uint64 {
 	return uint64(time.Now().UnixMilli())
 }
 
+// AddNewAccount function creates and adds new AccountInfo to suite accounts list. Returns index of new account in
+// the list and AccountInfo.
+func AddNewAccount(suite *f.BaseSuite, chainID proto.Scheme) (int, config.AccountInfo) {
+	seed := strconv.FormatInt(time.Now().UnixNano(), 10)
+	sk, pk, err := crypto.GenerateKeyPair([]byte(seed))
+	require.NoError(suite.T(), err)
+	addr, err := proto.NewAddressFromPublicKey(chainID, pk)
+	require.NoError(suite.T(), err)
+	acc := config.AccountInfo{
+		PublicKey: pk,
+		SecretKey: sk,
+		Amount:    0,
+		Address:   addr,
+	}
+	suite.Cfg.Accounts = append(suite.Cfg.Accounts, acc)
+	return len(suite.Cfg.Accounts) - 1, acc
+}
+
 func GetAccount(suite *f.BaseSuite, i int) config.AccountInfo {
+	if i < 0 || i > len(suite.Cfg.Accounts)-1 {
+		require.FailNow(suite.T(),
+			fmt.Sprintf("Invalid account index %d, should be between 0 and %d", i, len(suite.Cfg.Accounts)))
+	}
 	return suite.Cfg.Accounts[i]
+}
+
+func GetAddressByAliasGo(suite *f.BaseSuite, alias string) []byte {
+	return suite.Clients.GoClients.GrpcClient.GetAddressByAlias(suite.T(), alias)
+}
+
+func GetAddressByAliasScala(suite *f.BaseSuite, alias string) []byte {
+	return suite.Clients.ScalaClients.GrpcClient.GetAddressByAlias(suite.T(), alias)
+}
+
+func GetAddressesByAlias(suite *f.BaseSuite, alias string) ([]byte, []byte) {
+	return GetAddressByAliasGo(suite, alias), GetAddressByAliasScala(suite, alias)
 }
 
 func GetAvailableBalanceInWavesGo(suite *f.BaseSuite, address proto.WavesAddress) int64 {
@@ -58,27 +146,34 @@ func GetAvailableBalanceInWavesGo(suite *f.BaseSuite, address proto.WavesAddress
 }
 
 func GetAvailableBalanceInWavesScala(suite *f.BaseSuite, address proto.WavesAddress) int64 {
-	return suite.Clients.ScalaClients.GrpcClient.GetWavesBalance(suite.T(), address).GetAvailable()
+	//TODO(ipereiaslavskaia) return suite.Clients.ScalaClients.GrpcClient.GetWavesBalance(suite.T(), address).GetAvailable() after fixing grpc interface
+	wavesBalance := suite.Clients.ScalaClients.HttpClient.WavesBalance(suite.T(), address)
+	return int64(wavesBalance.Balance)
 }
 
 func GetAvailableBalanceInWaves(suite *f.BaseSuite, address proto.WavesAddress) (int64, int64) {
-	wavesBalanceGo := GetAvailableBalanceInWavesGo(suite, address)
-	wavesBalanceScala := GetAvailableBalanceInWavesScala(suite, address)
-	return wavesBalanceGo, wavesBalanceScala
+	return GetAvailableBalanceInWavesGo(suite, address), GetAvailableBalanceInWavesScala(suite, address)
 }
 
-func GetAssetBalanceGo(suite *f.BaseSuite, address proto.WavesAddress, id []byte) int64 {
-	return suite.Clients.GoClients.GrpcClient.GetAssetBalance(suite.T(), address, id).GetAmount()
+func GetAssetBalanceGo(suite *f.BaseSuite, address proto.WavesAddress, assetId crypto.Digest) int64 {
+	return suite.Clients.GoClients.GrpcClient.GetAssetBalance(suite.T(), address, assetId.Bytes()).GetAmount()
 }
 
-func GetAssetBalanceScala(suite *f.BaseSuite, address proto.WavesAddress, id []byte) int64 {
-	return suite.Clients.ScalaClients.GrpcClient.GetAssetBalance(suite.T(), address, id).GetAmount()
+func GetAssetBalanceScala(suite *f.BaseSuite, address proto.WavesAddress, assetId crypto.Digest) int64 {
+	//TODO (ipereiaslavskaia) return suite.Clients.ScalaClients.GrpcClient.GetAssetBalance(suite.T(), address, assetId.Bytes()).GetAmount() after fixing grpc interface
+	assetBalance := suite.Clients.ScalaClients.HttpClient.AssetBalance(suite.T(), address, assetId)
+	return int64(assetBalance.Balance)
 }
 
-func GetAssetBalance(suite *f.BaseSuite, address proto.WavesAddress, id []byte) (int64, int64) {
-	assetBalanceGo := GetAssetBalanceGo(suite, address, id)
-	assetBalanceScala := GetAssetBalanceScala(suite, address, id)
-	return assetBalanceGo, assetBalanceScala
+func GetAssetBalance(suite *f.BaseSuite, address proto.WavesAddress, assetId crypto.Digest) (int64, int64) {
+	return GetAssetBalanceGo(suite, address, assetId), GetAssetBalanceScala(suite, address, assetId)
+}
+
+func GetActualDiffBalanceInWaves(suite *f.BaseSuite, address proto.WavesAddress, initBalanceGo, initBalanceScala int64) (int64, int64) {
+	currentBalanceInWavesGo, currentBalanceInWavesScala := GetAvailableBalanceInWaves(suite, address)
+	actualDiffBalanceInWavesGo := initBalanceGo - currentBalanceInWavesGo
+	actualDiffBalanceInWavesScala := initBalanceScala - currentBalanceInWavesScala
+	return actualDiffBalanceInWavesGo, actualDiffBalanceInWavesScala
 }
 
 func GetTxIdsInBlockchain(suite *f.BaseSuite, ids map[string]*crypto.Digest,
@@ -120,7 +215,7 @@ func GetTxIdsInBlockchain(suite *f.BaseSuite, ids map[string]*crypto.Digest,
 	}
 }
 
-func extractTxID(t *testing.T, tx proto.Transaction, scheme proto.Scheme) crypto.Digest {
+func ExtractTxID(t *testing.T, tx proto.Transaction, scheme proto.Scheme) crypto.Digest {
 	idBytes, err := tx.GetID(scheme)
 	require.NoError(t, err, "failed to get txID")
 	id, err := crypto.NewDigestFromBytes(idBytes)
@@ -128,32 +223,54 @@ func extractTxID(t *testing.T, tx proto.Transaction, scheme proto.Scheme) crypto
 	return id
 }
 
-func marshalTransaction(t *testing.T, tx proto.Transaction) []byte {
-	bts, err := tx.MarshalBinary()
+func MarshalTxAndGetTxMsg(t *testing.T, scheme proto.Scheme, tx proto.Transaction) proto.Message {
+	bts, err := proto.MarshalTx(scheme, tx)
 	require.NoError(t, err, "failed to marshal tx")
-	return bts
+	t.Logf("Transaction bytes: %s", base64.StdEncoding.EncodeToString(bts))
+	if proto.IsProtobufTx(tx) {
+		return &proto.PBTransactionMessage{Transaction: bts}
+	} else {
+		return &proto.TransactionMessage{Transaction: bts}
+	}
+
 }
 
-func SendAndWaitTransaction(suite *f.BaseSuite, tx proto.Transaction, scheme proto.Scheme,
-	timeout time.Duration) (error, error) {
-	bts := marshalTransaction(suite.T(), tx)
-	id := extractTxID(suite.T(), tx, scheme)
-	txMsg := proto.TransactionMessage{Transaction: bts}
+func SendAndWaitTransaction(suite *f.BaseSuite, tx proto.Transaction, scheme proto.Scheme, timeout time.Duration,
+	positive bool) ConsideredTransaction {
+
+	id := ExtractTxID(suite.T(), tx, scheme)
+	txMsg := MarshalTxAndGetTxMsg(suite.T(), scheme, tx)
+	scala := !positive
+
+	suite.Clients.ClearBlackList(suite.T())
 
 	suite.Conns.Reconnect(suite.T(), suite.Ports)
-	suite.Conns.SendToEachNode(suite.T(), &txMsg)
 
+	suite.Conns.SendToNodes(suite.T(), txMsg, scala)
 	errGo, errScala := suite.Clients.WaitForTransaction(id, timeout)
-	return errGo, errScala
+	return *NewConsideredTransaction(id, nil, nil, errGo, errScala, nil, nil)
 }
 
-func BroadcastAndWaitTransaction(suite *f.BaseSuite, tx proto.Transaction, scheme proto.Scheme, timeout time.Duration) (
-	BroadcastedTransaction, error, error) {
-	id := extractTxID(suite.T(), tx, scheme)
-
+func BroadcastAndWaitTransaction(suite *f.BaseSuite, tx proto.Transaction, scheme proto.Scheme, timeout time.Duration,
+	positive bool) ConsideredTransaction {
+	id := ExtractTxID(suite.T(), tx, scheme)
 	respGo, errBrdCstGo := suite.Clients.GoClients.HttpClient.TransactionBroadcast(tx)
-	respScala, errBrdCstScala := suite.Clients.ScalaClients.HttpClient.TransactionBroadcast(tx)
+	var respScala *client.Response = nil
+	var errBrdCstScala error = nil
+	if !positive {
+		respScala, errBrdCstScala = suite.Clients.ScalaClients.HttpClient.TransactionBroadcast(tx)
+	}
 	errWtGo, errWtScala := suite.Clients.WaitForTransaction(id, timeout)
 
-	return *NewBroadcastedTransaction(id, respGo, errBrdCstGo, respScala, errBrdCstScala), errWtGo, errWtScala
+	return *NewConsideredTransaction(id, respGo, respScala, errWtGo, errWtScala, errBrdCstGo, errBrdCstScala)
+}
+
+func TransferFunds(suite *f.BaseSuite, scheme proto.Scheme, from, to int, amount int) ConsideredTransaction {
+	sender := GetAccount(suite, from)
+	recipient := GetAccount(suite, to)
+	tx := proto.NewUnsignedTransferWithSig(sender.PublicKey, proto.NewOptionalAssetWaves(), proto.NewOptionalAssetWaves(),
+		uint64(time.Now().UnixMilli()), uint64(amount), 100000, proto.NewRecipientFromAddress(recipient.Address), nil)
+	err := tx.Sign(scheme, sender.SecretKey)
+	require.NoError(suite.T(), err)
+	return SendAndWaitTransaction(suite, tx, scheme, 5*time.Second, true)
 }
