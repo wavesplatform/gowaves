259c83086454c9ecf43a008837b93192e648ee37
esuwu <sanya554455@gmail.com>
2022-12-28 01:33:15 -0600

    Added a test multiple account seeds

--- pkg/wallet/wallet_test.go
+++ pkg/wallet/wallet_test.go
@@ -24,3 +24,25 @@ func TestWallet_EncodeDecode(t *testing.T) {
 	_, err = Decode(bts, []byte("unknown password"))
 	require.Error(t, err)
 }
+
+func TestWallet_EncodeDecodeMultipleAccountSeeds(t *testing.T) {
+	password := []byte("123456")
+
+	w := NewWallet()
+	err := w.AddAccountSeed([]byte("BjABqgaAhXb13wAcjUNBv1hzaxDmzT9hdQC3uCzZ"))
+	require.NoError(t, err)
+	err = w.AddAccountSeed([]byte("MGZxEhfGteDuhGL2euXMt9UuopJH1x9ti52Nmz5uuigU4Wm"))
+	require.NoError(t, err)
+	err = w.AddAccountSeed([]byte("GmVmL79kzru97FKVnz8jKvpEFECwTxZv"))
+	require.NoError(t, err)
+
+	bts, err := w.Encode(password)
+	require.NoError(t, err)
+
+	w2, err := Decode(bts, password)
+	require.NoError(t, err)
+	assert.Equal(t, w.AccountSeeds(), w2.AccountSeeds())
+
+	_, err = Decode(bts, []byte("unknown password"))
+	require.Error(t, err)
+}
