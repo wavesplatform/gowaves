04c8d7f4d8d4db4c9dde1c3a8edd260f518a566e
irina-pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2023-01-18 11:56:49 +0300

    Api Transfer Tx Tests.

--- itests/transfer_tx_api_tx.go
+++ itests/transfer_tx_api_tx.go
@@ -1,6 +1,7 @@
 package itests
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -14,11 +15,11 @@ import (
 	"golang.org/x/exp/maps"
 )
 
-type TransferTxSuite struct {
+type TransferTxApiSuite struct {
 	f.BaseSuite
 }
 
-func (suite *TransferTxSuite) Test_TransferTxPositive() {
+func (suite *TransferTxApiSuite) Test_TransferTxApiPositive() {
 	versions := testdata.GetVersions()
 	waitForTx := true
 	for _, v := range versions {
@@ -28,14 +29,16 @@ func (suite *TransferTxSuite) Test_TransferTxPositive() {
 		alias_utilities.SetAliasToAccount(&suite.BaseSuite, v, testdata.TestChainID, alias, 3)
 		//выпускаем токен, который будем переводить другому аккаунту
 		reissuable := testdata.GetCommonIssueData(&suite.BaseSuite).Reissuable
-		itx := issue_utilities.IssueSendWithTestData(&suite.BaseSuite, reissuable, v, waitForTx)
+		itx := issue_utilities.IssueBroadcastWithTestData(&suite.BaseSuite, reissuable, v, waitForTx)
 		//используя новый токен, создаем тестовые данные для проверки транзакции перевода
 		tdmatrix := testdata.GetTransferPositiveData(&suite.BaseSuite, itx.TxID, alias)
 		for name, td := range tdmatrix {
 			suite.T().Run(name, func(t *testing.T) {
 				//выпускаем транзакцию перевода
-				tx, diffBalancesSender, diffBalancesRecipient := transfer_utilities.SendTransferTxAndGetBalances(
+				tx, diffBalancesSender, diffBalancesRecipient := transfer_utilities.BroadcastTransferTxAndGetBalances(
 					&suite.BaseSuite, td, v, waitForTx)
+
+				utl.StatusCodesCheck(suite.T(), http.StatusOK, http.StatusOK, tx, name, "version", v)
 				utl.TxInfoCheck(suite.T(), tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, name,
 					"Transfer: "+tx.TxID.String(), "Version: ", v)
 				//проверяем балансы аккаунтов
@@ -60,50 +63,7 @@ func (suite *TransferTxSuite) Test_TransferTxPositive() {
 	}
 }
 
-// TODO need to fix transfer , it should be 2 transfers
-func (suite *TransferTxSuite) Test_TransferTxMaxAmountAndFeePositive() {
-	versions := testdata.GetVersions()
-	waitForTx := true
-	for _, v := range versions {
-		//создаем новый аккаунт с ненулевым балансом
-		n := transfer_utilities.GetNewAccountWithFunds(&suite.BaseSuite, v, testdata.TestChainID, 9, 10000000000)
-		//выпускаем токен, который будем переводить другому аккаунту
-		reissuable := testdata.GetCommonIssueData(&suite.BaseSuite).Reissuable
-		//itxID := issue_utilities.IssueAssetAmount(&suite.BaseSuite, v, testdata.TestChainID, 2)
-		itx := issue_utilities.IssueSendWithTestData(&suite.BaseSuite, reissuable, v, waitForTx)
-		//переводим токен с аккаунта эмитента на новый аккаунт
-		transfer_utilities.TransferAssetAmount(&suite.BaseSuite, v, testdata.TestChainID, itx.TxID, 2, n)
-		//используя новый токен, создаем тестовые данные для проверки транзакции перевода
-		tdmatrix := testdata.GetTransferMaxAmountPositive(&suite.BaseSuite, itx.TxID, n)
-		for name, td := range tdmatrix {
-			//suite.T().Run(name, func(t *testing.T) {})
-			//выпускаем транзакцию перевода
-			tx, diffBalancesSender, diffBalancesRecipient := transfer_utilities.SendTransferTxAndGetBalances(
-				&suite.BaseSuite, td, v, waitForTx)
-			utl.TxInfoCheck(suite.T(), tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, name,
-				"Transfer: "+tx.TxID.String(), "Version: ", v)
-			//проверяем балансы аккаунтов
-			//баланс вавесов аккаунта, с которого переводят, уменьшается на комиссию
-			utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalanceSender,
-				diffBalancesSender.DiffBalanceWaves.BalanceInWavesGo,
-				diffBalancesSender.DiffBalanceWaves.BalanceInWavesScala, name, "Version: ", v)
-			//баланс ассетов аккаунта, с которого переводят, уменьшается на количество токена, переводимое другому аккаунту
-			utl.AssetBalanceCheck(suite.T(), td.Expected.AssetDiffBalance,
-				diffBalancesSender.DiffBalanceAsset.BalanceInAssetGo,
-				diffBalancesSender.DiffBalanceAsset.BalanceInAssetScala, name, "Version: ", v)
-			//баланс вавесов аккаунта, на который переводят токен, не меняется
-			utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalanceRecipient,
-				diffBalancesRecipient.DiffBalanceWaves.BalanceInWavesGo,
-				diffBalancesRecipient.DiffBalanceWaves.BalanceInWavesScala, name, "Version: ", v)
-			//баланс ассетов аккаунта, на который переводят, увеличивается на количество переводимого токена
-			utl.AssetBalanceCheck(suite.T(), td.Expected.AssetDiffBalance,
-				diffBalancesRecipient.DiffBalanceAsset.BalanceInAssetGo,
-				diffBalancesRecipient.DiffBalanceAsset.BalanceInAssetScala, name, "Version: ", v)
-		}
-	}
-}
-
-func (suite *TransferTxSuite) Test_TransferTxNegative() {
+func (suite *TransferTxApiSuite) Test_TransferTxApiNegative() {
 	versions := testdata.GetVersions()
 	waitForTx := true
 	for _, v := range versions {
@@ -122,8 +82,14 @@ func (suite *TransferTxSuite) Test_TransferTxNegative() {
 				//выпускаем транзакцию перевода
 				tx, diffBalancesSender, diffBalancesRecipient := transfer_utilities.SendTransferTxAndGetBalances(
 					&suite.BaseSuite, td, v, !waitForTx)
+				utl.StatusCodesCheck(suite.T(), http.StatusInternalServerError, http.StatusBadRequest, tx, name, "version", v)
+				utl.ErrorMessageCheck(suite.T(), td.Expected.ErrBrdCstGoMsg, td.Expected.ErrBrdCstScalaMsg,
+					tx.BrdCstErr.ErrorBrdCstGo, tx.BrdCstErr.ErrorBrdCstScala, name, "version", v)
+
 				txIds[name] = &tx.TxID
 				//проверяем балансы аккаунтов
+
+				utl.ErrorMessageCheck(suite.T(), td.Expected.ErrGoMsg, td.Expected.ErrScalaMsg, tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala)
 				//баланс вавесов аккаунта, с которого переводят не меняется
 				utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalance,
 					diffBalancesSender.DiffBalanceWaves.BalanceInWavesGo,
@@ -151,7 +117,7 @@ func (suite *TransferTxSuite) Test_TransferTxNegative() {
 	}
 }
 
-func TestTransferTxSuite(t *testing.T) {
+func TestTransferTxApiSuite(t *testing.T) {
 	t.Parallel()
-	suite.Run(t, new(TransferTxSuite))
+	suite.Run(t, new(TransferTxApiSuite))
 }
