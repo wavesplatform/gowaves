b632ff48b6d77c862c80a9fc93256b6d55b5e539
Alexey Kiselev <alexey.kiselev@gmail.com>
2023-05-10 11:37:20 +0400

    Fixed sorting of rewards with stable sort.

--- pkg/proto/reward.go
+++ pkg/proto/reward.go
@@ -26,7 +26,7 @@ type Rewards []Reward
 
 func (r *Rewards) Sorted() Rewards {
 	rewards := *r
-	sort.Sort(rewardsByAddress(rewards))
+	sort.Stable(rewardsByAddress(rewards))
 	return rewards
 }
 
@@ -36,9 +36,10 @@ func (r rewardsByAddress) Len() int      { return len(r) }
 func (r rewardsByAddress) Swap(i, j int) { r[i], r[j] = r[j], r[i] }
 
 func (r rewardsByAddress) Less(i, j int) bool {
-	// TODO: Consider adding second sort order by amount
-	//if r[i].address.Equal(r[j].address) {
-	//	return r[i].amount < r[j].amount
-	//}
+	//TODO: Consider adding second sort order by amount, which is not scala implementation compatible for now.
+	// In scala implementation they depend on stability of sort and add miner on the last position.
+	// if r[i].address.Equal(r[j].address) {
+	//	 return r[i].amount < r[j].amount
+	// }
 	return bytes.Compare(r[i].address.Bytes(), r[j].address.Bytes()) < 0
 }
