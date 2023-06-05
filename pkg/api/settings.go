2f1c5da1afd294906841c23b2b764d2f4117c386
Alexey Kiselev <alexey.kiselev@gmail.com>
2023-06-05 14:18:11 +0400

    Set default rate limiter options (#1112)
    * Set meaningful default values for API rate limieter.
    
    * Added command line parameter to configure API rate limiter.
    Increased smallest timeout in itests up to 50ms.
    Loose rate limiter in itests.
    
    * Reduce initial integration test timeout to 5ms.
    Fixed unclear logic in rate limiter parameters parsing.

--- pkg/api/settings.go
+++ pkg/api/settings.go
@@ -2,12 +2,25 @@ package api
 
 import (
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
 const (
-	DefaultMaxConnections = 128
+	DefaultMaxConnections       = 128
+	DefaultRateLimiterCacheSize = 64 * 1024 // 64 KB
+	DefaultRateLimiterRPS       = 1
+	DefaultRateLimiterBurst     = 1
+)
+
+const (
+	cacheSizeKey = "cache"
+	rpsKey       = "rps"
+	burstKey     = "burst"
 )
 
 type RunOptions struct {
@@ -31,7 +44,7 @@ type RateLimiterOptions struct {
 
 func DefaultRunOptions() *RunOptions {
 	return &RunOptions{
-		RateLimiterOpts:      nil,
+		RateLimiterOpts:      DefaultRateLimiterOptions(),
 		LogHttpRequestOpts:   false,
 		EnableHeartbeatRoute: true,
 		UseRealIPMiddleware:  true,
@@ -45,4 +58,51 @@ func DefaultRunOptions() *RunOptions {
 		EnableMetaMaskAPI:    false,
 		EnableMetaMaskAPILog: false,
 	}
+}
+
+func DefaultRateLimiterOptions() *RateLimiterOptions {
+	return &RateLimiterOptions{
+		MemoryCacheSize:      DefaultRateLimiterCacheSize,
+		MaxRequestsPerSecond: DefaultRateLimiterRPS,
+		MaxBurst:             DefaultRateLimiterBurst,
+	}
+}
+
+func NewRateLimiterOptionsFromString(s string) (*RateLimiterOptions, error) {
+	opt := DefaultRateLimiterOptions()
+	query, err := url.ParseQuery(strings.TrimSpace(s))
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid rate limiter options")
+	}
+	cacheSize, err := extractFirstIntValue(query, cacheSizeKey, DefaultRateLimiterCacheSize)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid rate limiter options")
+	}
+	opt.MemoryCacheSize = cacheSize
+	rps, err := extractFirstIntValue(query, rpsKey, DefaultRateLimiterRPS)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid rate limiter options")
+	}
+	opt.MaxRequestsPerSecond = rps
+	burst, err := extractFirstIntValue(query, burstKey, DefaultRateLimiterBurst)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid rate limiter options")
+	}
+	opt.MaxBurst = burst
+	return opt, nil
+}
+
+func extractFirstIntValue(query url.Values, key string, dft int) (int, error) {
+	values, ok := query[key]
+	if !ok {
+		return dft, nil
+	}
+	if len(values) < 1 {
+		return 0, errors.Errorf("no value for key '%s'", key)
+	}
+	v, err := strconv.ParseInt(strings.TrimSpace(values[0]), 10, 32)
+	if err != nil {
+		return 0, errors.Wrapf(err, "invalid value for key '%s'", key)
+	}
+	return int(v), nil
 }
