23ccec437a044c562a309a95721aae89b99f8fc7
Nikolay Eskov <mr.eskov1@yandex.ru>
2023-06-12 16:42:22 +0300

    Add 'ITESTS_KEEP_DANGLING' environment variable.

--- itests/init_test.go
+++ itests/init_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"github.com/ory/dockertest/v3"
@@ -12,15 +13,18 @@ import (
 )
 
 const dockerfilePath = "/../Dockerfile.gowaves-it"
+const keepDanglingEnvKey = "ITESTS_KEEP_DANGLING"
 
 func TestMain(m *testing.M) {
 	pwd, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("%s", err)
+		log.Fatalf("Failed to get pwd: %v", err)
 	}
+	keepDangling := mustBoolEnv(keepDanglingEnvKey)
+
 	pool, err := dockertest.NewPool("")
 	if err != nil {
-		log.Fatalf("Failed to create docker pool: %s", err)
+		log.Fatalf("Failed to create docker pool: %v", err)
 	}
 	dir, file := filepath.Split(pwd + dockerfilePath)
 	err = pool.Client.BuildImage(dc.BuildImageOptions{
@@ -33,23 +37,36 @@ func TestMain(m *testing.M) {
 		RmTmpContainer: true,
 	})
 	if err != nil {
-		log.Fatalf("Failed to create go-node image: %s", err)
+		log.Fatalf("Failed to create go-node image: %v", err)
 	}
 
-	// remove dangling images
-	imgs, err := pool.Client.ListImages(dc.ListImagesOptions{
-		Filters: map[string][]string{
-			"label": {"wavesplatform-gowaves-itests-tmp=true"},
-		},
-	})
-	if err != nil {
-		log.Fatalf("Failed to get list of images from docker: %s", err)
-	}
-	for _, i := range imgs {
-		err = pool.Client.RemoveImage(i.ID)
+	if !keepDangling { // remove dangling images
+		imgs, err := pool.Client.ListImages(dc.ListImagesOptions{
+			Filters: map[string][]string{
+				"label": {"wavesplatform-gowaves-itests-tmp=true"},
+			},
+		})
 		if err != nil {
-			log.Fatalf("Failed to remove dangling images: %s", err)
+			log.Fatalf("Failed to get list of images from docker: %v", err)
+		}
+		for _, i := range imgs {
+			err = pool.Client.RemoveImage(i.ID)
+			if err != nil {
+				log.Fatalf("Failed to remove dangling images: %v", err)
+			}
 		}
 	}
 	os.Exit(m.Run())
+}
+
+func mustBoolEnv(key string) bool {
+	envFlag := os.Getenv(key)
+	if envFlag == "" {
+		return false
+	}
+	r, err := strconv.ParseBool(envFlag)
+	if err != nil {
+		log.Fatalf("Invalid flag value %q for the env key %q: %v", envFlag, key, err)
+	}
+	return r
 }
