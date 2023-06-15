ddbaa2fd9ecb49cedc50785606490cb66b3aa3a4
esuwu <sanya554455@gmail.com>
2023-06-15 01:54:34 -0500

    Fixed tests

--- pkg/state/transaction_snapshot_test.go
+++ pkg/state/transaction_snapshot_test.go
@@ -2,11 +2,11 @@ package state
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/wavesplatform/gowaves/pkg/proto"
 	"github.com/wavesplatform/gowaves/pkg/settings"
 	"math/big"
+	"sort"
 	"testing"
 )
 
@@ -44,15 +44,40 @@ func TestDefaultTransferWavesAndAssetSnapshot(t *testing.T) {
 	assert.NoError(t, err, "createDiffTransferWithSig() failed")
 	applicationRes := &applicationResult{true, 0, ch}
 	transactionSnapshot, err := to.tp.performTransferWithSig(tx, defaultPerformerInfo(), nil, applicationRes)
-
-	for _, s := range transactionSnapshot {
-		b, _ := json.Marshal(s)
-		fmt.Println(string(b))
+	assert.NoError(t, err, "failed to perform transfer tx")
+	expectedSnapshot := TransactionSnapshot{
+		&WavesBalanceSnapshot{
+			Address: testGlobal.minerInfo.addr,
+			Balance: 40000,
+		},
+		&WavesBalanceSnapshot{
+			Address: testGlobal.issuerInfo.addr,
+			Balance: 299700000,
+		},
+		&WavesBalanceSnapshot{
+			Address: testGlobal.recipientInfo.addr,
+			Balance: 200000,
+		},
 	}
 
-	assert.NoError(t, err, "performIssueWithProofs() failed")
+	sort.Slice(expectedSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(expectedSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(expectedSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	sort.Slice(transactionSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(transactionSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(transactionSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	assert.Equal(t, expectedSnapshot, transactionSnapshot)
 	to.stor.flush(t)
-	assert.NotNil(t, transactionSnapshot)
 }
 
 // TODO send only txBalanceChanges to perfomer
@@ -63,9 +88,6 @@ func TestDefaultIssueTransactionSnapshot(t *testing.T) {
 	to.stor.activateFeature(t, int16(settings.NG))
 	err := to.stor.entities.balances.setWavesBalance(testGlobal.issuerInfo.addr.ID(), &wavesValue{profile: balanceProfile{balance: 1000 * FeeUnit * 3}}, blockID0)
 	assert.NoError(t, err, "failed to set waves balance")
-	err = to.stor.entities.balances.setAssetBalance(testGlobal.issuerInfo.addr.ID(), proto.AssetIDFromDigest(testGlobal.asset0.assetID), 1000000000, blockID0)
-	assert.NoError(t, err, "failed to set waves balance")
-
 	tx := proto.NewUnsignedIssueWithSig(testGlobal.issuerInfo.pk, "asset0", "description", defaultQuantity, defaultDecimals, true, defaultTimestamp, uint64(1*FeeUnit))
 	err = tx.Sign(proto.TestNetScheme, testGlobal.issuerInfo.sk)
 	assert.NoError(t, err, "failed to sign issue tx")
@@ -74,12 +96,8 @@ func TestDefaultIssueTransactionSnapshot(t *testing.T) {
 	assert.NoError(t, err, "createDiffIssueWithSig() failed")
 	applicationRes := &applicationResult{true, 0, ch}
 	transactionSnapshot, err := to.tp.performIssueWithSig(tx, defaultPerformerInfo(), nil, applicationRes)
-	assert.NoError(t, err, "performIssueWithProofs() failed")
-	for _, s := range transactionSnapshot {
-		b, _ := json.Marshal(s)
-		fmt.Println(string(b))
-	}
-	fmt.Println(tx.ID.String())
+	assert.NoError(t, err, "failed to perform issue tx")
+
 	expectedSnapshot := TransactionSnapshot{
 		&StaticAssetInfoSnapshot{
 			AssetID:             *tx.ID,
@@ -112,6 +130,21 @@ func TestDefaultIssueTransactionSnapshot(t *testing.T) {
 			Balance: 1000,
 		},
 	}
+	sort.Slice(expectedSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(expectedSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(expectedSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	sort.Slice(transactionSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(transactionSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(transactionSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
 
 	assert.Equal(t, expectedSnapshot, transactionSnapshot)
 	to.stor.flush(t)
@@ -122,29 +155,62 @@ func TestDefaultReissueSnapshot(t *testing.T) {
 
 	to.stor.addBlock(t, blockID0)
 	to.stor.activateFeature(t, int16(settings.NG))
-	err := to.stor.entities.assets.issueAsset(proto.AssetIDFromDigest(testGlobal.asset0.assetID), defaultAssetInfo(proto.DigestTail(testGlobal.asset0.assetID), true), blockID0)
+	err := to.stor.entities.assets.issueAsset(proto.AssetIDFromDigest(testGlobal.asset0.assetID), defaultAssetInfoTransfer(proto.DigestTail(testGlobal.asset0.assetID), true, 1000), blockID0)
 	assert.NoError(t, err, "failed to issue asset")
 	err = to.stor.entities.balances.setWavesBalance(testGlobal.issuerInfo.addr.ID(), &wavesValue{profile: balanceProfile{balance: 1000 * FeeUnit * 3}}, blockID0)
 	assert.NoError(t, err, "failed to set waves balance")
 	err = to.stor.entities.balances.setAssetBalance(testGlobal.issuerInfo.addr.ID(), proto.AssetIDFromDigest(testGlobal.asset0.assetID), 1000, blockID0)
 	assert.NoError(t, err, "failed to set waves balance")
 
-	tx := proto.NewUnsignedReissueWithSig(testGlobal.issuerInfo.pk, testGlobal.asset0.assetID, 50, false, defaultTimestamp, uint64(FeeUnit))
+	tx := proto.NewUnsignedReissueWithSig(testGlobal.issuerInfo.pk, testGlobal.asset0.assetID, 50, false, defaultTimestamp, uint64(1*FeeUnit))
 	err = tx.Sign(proto.TestNetScheme, testGlobal.issuerInfo.sk)
-	assert.NoError(t, err, "failed to sign transfer tx")
+	assert.NoError(t, err, "failed to sign reissue tx")
 
 	ch, err := to.td.createDiffReissueWithSig(tx, defaultDifferInfo())
-	assert.NoError(t, err, "createDiffTransferWithSig() failed")
+	assert.NoError(t, err, "createDiffReissueWithSig() failed")
 	applicationRes := &applicationResult{true, 0, ch}
 	transactionSnapshot, err := to.tp.performReissueWithSig(tx, defaultPerformerInfo(), nil, applicationRes)
+	assert.NoError(t, err, "failed to perform reissue tx")
 
-	for _, s := range transactionSnapshot {
-		b, _ := json.Marshal(s)
-		fmt.Println(string(b))
+	expectedSnapshot := TransactionSnapshot{
+		&WavesBalanceSnapshot{
+			Address: testGlobal.minerInfo.addr,
+			Balance: 40000,
+		},
+		&WavesBalanceSnapshot{
+			Address: testGlobal.issuerInfo.addr,
+			Balance: 299900000,
+		},
+		&AssetBalanceSnapshot{
+			Address: testGlobal.issuerInfo.addr,
+			AssetID: testGlobal.asset0.assetID,
+			Balance: 1050,
+		},
+		&AssetVolumeSnapshot{
+			AssetID:       testGlobal.asset0.assetID,
+			TotalQuantity: *big.NewInt(int64(defaultQuantity + 50)),
+			IsReissuable:  false,
+		},
 	}
-	assert.NoError(t, err, "performIssueWithProofs() failed")
+
+	sort.Slice(expectedSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(expectedSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(expectedSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	sort.Slice(transactionSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(transactionSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(transactionSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	assert.Equal(t, expectedSnapshot, transactionSnapshot)
 	to.stor.flush(t)
-	assert.NotNil(t, transactionSnapshot)
 }
 
 func TestDefaultBurnSnapshot(t *testing.T) {
@@ -152,29 +218,60 @@ func TestDefaultBurnSnapshot(t *testing.T) {
 
 	to.stor.addBlock(t, blockID0)
 	to.stor.activateFeature(t, int16(settings.NG))
-	err := to.stor.entities.assets.issueAsset(proto.AssetIDFromDigest(testGlobal.asset0.assetID), defaultAssetInfo(proto.DigestTail(testGlobal.asset0.assetID), true), blockID0)
-	//fmt.Println(testGlobal.issuerInfo.addr.String())
-	//fmt.Println(testGlobal.issuerInfo.pk.String())
+	err := to.stor.entities.assets.issueAsset(proto.AssetIDFromDigest(testGlobal.asset0.assetID), defaultAssetInfoTransfer(proto.DigestTail(testGlobal.asset0.assetID), true, 1000), blockID0)
+
 	assert.NoError(t, err, "failed to issue asset")
 	err = to.stor.entities.balances.setWavesBalance(testGlobal.issuerInfo.addr.ID(), &wavesValue{profile: balanceProfile{balance: 1000 * FeeUnit * 3}}, blockID0)
 	assert.NoError(t, err, "failed to set waves balance")
-	err = to.stor.entities.balances.setAssetBalance(testGlobal.issuerInfo.addr.ID(), proto.AssetIDFromDigest(testGlobal.asset0.assetID), 1000000000, blockID0)
-	assert.NoError(t, err, "failed to set waves balance")
+	err = to.stor.entities.balances.setAssetBalance(testGlobal.issuerInfo.addr.ID(), proto.AssetIDFromDigest(testGlobal.asset0.assetID), 1000, blockID0)
+	assert.NoError(t, err, "failed to set asset balance")
 
-	tx := proto.NewUnsignedBurnWithSig(testGlobal.issuerInfo.pk, testGlobal.asset0.assetID, 50, defaultTimestamp, uint64(1000*FeeUnit))
+	tx := proto.NewUnsignedBurnWithSig(testGlobal.issuerInfo.pk, testGlobal.asset0.assetID, 50, defaultTimestamp, uint64(1*FeeUnit))
 	err = tx.Sign(proto.TestNetScheme, testGlobal.issuerInfo.sk)
-	assert.NoError(t, err, "failed to sign transfer tx")
-	fmt.Println(testGlobal.asset0.assetID)
+	assert.NoError(t, err, "failed to sign burn tx")
 	ch, err := to.td.createDiffBurnWithSig(tx, defaultDifferInfo())
-	assert.NoError(t, err, "createDiffTransferWithSig() failed")
+	assert.NoError(t, err, "createDiffBurnWithSig() failed")
 	applicationRes := &applicationResult{true, 0, ch}
 	transactionSnapshot, err := to.tp.performBurnWithSig(tx, defaultPerformerInfo(), nil, applicationRes)
+	assert.NoError(t, err, "failed to perform burn tx")
 
-	for _, s := range transactionSnapshot {
-		b, _ := json.Marshal(s)
-		fmt.Println(string(b))
+	expectedSnapshot := TransactionSnapshot{
+		&WavesBalanceSnapshot{
+			Address: testGlobal.minerInfo.addr,
+			Balance: 40000,
+		},
+		&WavesBalanceSnapshot{
+			Address: testGlobal.issuerInfo.addr,
+			Balance: 299900000,
+		},
+		&AssetBalanceSnapshot{
+			Address: testGlobal.issuerInfo.addr,
+			AssetID: testGlobal.asset0.assetID,
+			Balance: 950,
+		},
+		&AssetVolumeSnapshot{
+			AssetID:       testGlobal.asset0.assetID,
+			TotalQuantity: *big.NewInt(int64(defaultQuantity - 50)),
+			IsReissuable:  true,
+		},
 	}
-	assert.NoError(t, err, "performIssueWithProofs() failed")
+
+	sort.Slice(expectedSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(expectedSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(expectedSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	sort.Slice(transactionSnapshot, func(i, j int) bool {
+		snapshotI, err := json.Marshal(transactionSnapshot[i])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		snapshotJ, err := json.Marshal(transactionSnapshot[j])
+		assert.NoError(t, err, "failed to marshal snapshots")
+		return string(snapshotI) < string(snapshotJ)
+	})
+
+	assert.Equal(t, expectedSnapshot, transactionSnapshot)
 	to.stor.flush(t)
-	assert.NotNil(t, transactionSnapshot)
 }
