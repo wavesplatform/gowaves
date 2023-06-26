483496bdf351ea75659c0eabbdd3fc605ec8e993
Nikolay Eskov <mr.eskov1@yandex.ru>
2023-06-26 22:49:49 +0300

    Extracted 'snapshotApplierStorages'.

--- pkg/state/snapshot_applier.go
+++ pkg/state/snapshot_applier.go
@@ -25,7 +25,16 @@ type SnapshotApplier interface {
 
 type blockSnapshotsApplier struct {
 	info blockSnapshotsApplierInfo
+	stor snapshotApplierStorages
+}
 
+var _ = newBlockSnapshotsApplier // TODO: only for linter, will be removed later
+
+func newBlockSnapshotsApplier(info blockSnapshotsApplierInfo, stor snapshotApplierStorages) blockSnapshotsApplier {
+	return blockSnapshotsApplier{info: info, stor: stor}
+}
+
+type snapshotApplierStorages struct {
 	balances          *balances
 	aliases           *aliases
 	assets            *assets
@@ -37,12 +46,38 @@ type blockSnapshotsApplier struct {
 	leases            *leases
 }
 
+var _ = newSnapshotApplierStorages // TODO: only for linter, will be removed later
+
+func newSnapshotApplierStorages(stor *blockchainEntitiesStorage) snapshotApplierStorages {
+	return snapshotApplierStorages{
+		balances:          stor.balances,
+		aliases:           stor.aliases,
+		assets:            stor.assets,
+		scriptsStorage:    stor.scriptsStorage,
+		scriptsComplexity: stor.scriptsComplexity,
+		sponsoredAssets:   stor.sponsoredAssets,
+		ordersVolumes:     stor.ordersVolumes,
+		accountsDataStor:  stor.accountsDataStor,
+		leases:            stor.leases,
+	}
+}
+
 var _ = SnapshotApplier((*blockSnapshotsApplier)(nil))
 
 type blockSnapshotsApplierInfo struct {
 	ci                  *checkerInfo
 	scheme              proto.Scheme
 	stateActionsCounter *proto.StateActionsCounter
+}
+
+var _ = newBlockSnapshotsApplierInfo
+
+func newBlockSnapshotsApplierInfo(ci *checkerInfo, scheme proto.Scheme, cnt *proto.StateActionsCounter) blockSnapshotsApplierInfo {
+	return blockSnapshotsApplierInfo{
+		ci:                  ci,
+		scheme:              scheme,
+		stateActionsCounter: cnt,
+	}
 }
 
 func (s blockSnapshotsApplierInfo) BlockID() proto.BlockID {
@@ -67,14 +102,14 @@ func (s blockSnapshotsApplierInfo) StateActionsCounter() *proto.StateActionsCoun
 
 func (a *blockSnapshotsApplier) ApplyWavesBalance(snapshot WavesBalanceSnapshot) error {
 	addrID := snapshot.Address.ID()
-	profile, err := a.balances.wavesBalance(addrID)
+	profile, err := a.stor.balances.wavesBalance(addrID)
 	if err != nil {
 		return errors.Wrapf(err, "failed to get waves balance profile for address %q", snapshot.Address.String())
 	}
 	newProfile := profile
 	newProfile.balance = snapshot.Balance
 	value := newWavesValue(profile, newProfile)
-	if err := a.balances.setWavesBalance(addrID, value, a.info.BlockID()); err != nil {
+	if err := a.stor.balances.setWavesBalance(addrID, value, a.info.BlockID()); err != nil {
 		return errors.Wrapf(err, "failed to get set balance profile for address %q", snapshot.Address.String())
 	}
 	return nil
@@ -82,7 +117,7 @@ func (a *blockSnapshotsApplier) ApplyWavesBalance(snapshot WavesBalanceSnapshot)
 
 func (a *blockSnapshotsApplier) ApplyLeaseBalance(snapshot LeaseBalanceSnapshot) error {
 	addrID := snapshot.Address.ID()
-	profile, err := a.balances.wavesBalance(addrID)
+	profile, err := a.stor.balances.wavesBalance(addrID)
 	if err != nil {
 		return errors.Wrapf(err, "failed to get waves balance profile for address %q", snapshot.Address.String())
 	}
@@ -90,7 +125,7 @@ func (a *blockSnapshotsApplier) ApplyLeaseBalance(snapshot LeaseBalanceSnapshot)
 	newProfile.leaseIn = int64(snapshot.LeaseIn)
 	newProfile.leaseOut = int64(snapshot.LeaseOut)
 	value := newWavesValue(profile, newProfile)
-	if err := a.balances.setWavesBalance(addrID, value, a.info.BlockID()); err != nil {
+	if err := a.stor.balances.setWavesBalance(addrID, value, a.info.BlockID()); err != nil {
 		return errors.Wrapf(err, "failed to get set balance profile for address %q", snapshot.Address.String())
 	}
 	return nil
@@ -99,11 +134,11 @@ func (a *blockSnapshotsApplier) ApplyLeaseBalance(snapshot LeaseBalanceSnapshot)
 func (a *blockSnapshotsApplier) ApplyAssetBalance(snapshot AssetBalanceSnapshot) error {
 	addrID := snapshot.Address.ID()
 	assetID := proto.AssetIDFromDigest(snapshot.AssetID)
-	return a.balances.setAssetBalance(addrID, assetID, snapshot.Balance, a.info.BlockID())
+	return a.stor.balances.setAssetBalance(addrID, assetID, snapshot.Balance, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyAlias(snapshot AliasSnapshot) error {
-	return a.aliases.createAlias(snapshot.Alias.Alias, snapshot.Address, a.info.BlockID())
+	return a.stor.aliases.createAlias(snapshot.Alias.Alias, snapshot.Address, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyStaticAssetInfo(snapshot StaticAssetInfoSnapshot) error {
@@ -118,7 +153,7 @@ func (a *blockSnapshotsApplier) ApplyStaticAssetInfo(snapshot StaticAssetInfoSna
 		},
 		assetChangeableInfo: assetChangeableInfo{},
 	}
-	return a.assets.issueAsset(assetID, assetFullInfo, a.info.BlockID())
+	return a.stor.assets.issueAsset(assetID, assetFullInfo, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyAssetDescription(snapshot AssetDescriptionSnapshot) error {
@@ -127,18 +162,18 @@ func (a *blockSnapshotsApplier) ApplyAssetDescription(snapshot AssetDescriptionS
 		newDescription: snapshot.AssetDescription,
 		newHeight:      snapshot.ChangeHeight,
 	}
-	return a.assets.updateAssetInfo(snapshot.AssetID, change, a.info.BlockID())
+	return a.stor.assets.updateAssetInfo(snapshot.AssetID, change, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyAssetVolume(snapshot AssetVolumeSnapshot) error {
 	assetID := proto.AssetIDFromDigest(snapshot.AssetID)
-	assetFullInfo, err := a.assets.newestAssetInfo(assetID)
+	assetFullInfo, err := a.stor.assets.newestAssetInfo(assetID)
 	if err != nil {
 		return errors.Wrapf(err, "failed to get newest asset info for asset %q", snapshot.AssetID.String())
 	}
 	assetFullInfo.assetChangeableInfo.reissuable = snapshot.IsReissuable
 	assetFullInfo.assetChangeableInfo.quantity = snapshot.TotalQuantity
-	return a.assets.storeAssetInfo(assetID, assetFullInfo, a.info.BlockID())
+	return a.stor.assets.storeAssetInfo(assetID, assetFullInfo, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyAssetScript(snapshot AssetScriptSnapshot) error {
@@ -147,18 +182,18 @@ func (a *blockSnapshotsApplier) ApplyAssetScript(snapshot AssetScriptSnapshot) e
 		Verifier:   int(snapshot.Complexity),
 		Functions:  nil,
 	}
-	if err := a.scriptsComplexity.saveComplexitiesForAsset(snapshot.AssetID, estimation, a.info.BlockID()); err != nil {
+	if err := a.stor.scriptsComplexity.saveComplexitiesForAsset(snapshot.AssetID, estimation, a.info.BlockID()); err != nil {
 		return errors.Wrapf(err, "failed to store asset script estimation for asset %q", snapshot.AssetID.String())
 	}
-	constInfo, err := a.assets.newestConstInfo(proto.AssetIDFromDigest(snapshot.AssetID)) // only issuer can set new asset script
+	constInfo, err := a.stor.assets.newestConstInfo(proto.AssetIDFromDigest(snapshot.AssetID)) // only issuer can set new asset script
 	if err != nil {
 		return errors.Wrapf(err, "failed to get const asset info for asset %q", snapshot.AssetID.String())
 	}
-	return a.scriptsStorage.setAssetScript(snapshot.AssetID, snapshot.Script, constInfo.issuer, a.info.BlockID())
+	return a.stor.scriptsStorage.setAssetScript(snapshot.AssetID, snapshot.Script, constInfo.issuer, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplySponsorship(snapshot SponsorshipSnapshot) error {
-	return a.sponsoredAssets.sponsorAsset(snapshot.AssetID, snapshot.MinSponsoredFee, a.info.BlockID())
+	return a.stor.sponsoredAssets.sponsorAsset(snapshot.AssetID, snapshot.MinSponsoredFee, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyAccountScript(snapshot AccountScriptSnapshot) error {
@@ -178,20 +213,20 @@ func (a *blockSnapshotsApplier) ApplyAccountScript(snapshot AccountScriptSnapsho
 			return errors.Wrapf(err, "failed to make account script estimations for addr %q", addr.String())
 		}
 	}
-	if err := a.scriptsComplexity.saveComplexitiesForAddr(addr, estimations, a.info.BlockID()); err != nil {
+	if err := a.stor.scriptsComplexity.saveComplexitiesForAddr(addr, estimations, a.info.BlockID()); err != nil {
 		return errors.Wrapf(err, "failed to store account script estimation for addr %q", addr.String())
 	}
-	return a.scriptsStorage.setAccountScript(addr, snapshot.Script, snapshot.SenderPublicKey, a.info.BlockID())
+	return a.stor.scriptsStorage.setAccountScript(addr, snapshot.Script, snapshot.SenderPublicKey, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyFilledVolumeAndFee(snapshot FilledVolumeFeeSnapshot) error {
-	return a.ordersVolumes.increaseFilled(snapshot.OrderID.Bytes(), snapshot.FilledVolume, snapshot.FilledFee, a.info.BlockID())
+	return a.stor.ordersVolumes.increaseFilled(snapshot.OrderID.Bytes(), snapshot.FilledVolume, snapshot.FilledFee, a.info.BlockID())
 }
 
 func (a *blockSnapshotsApplier) ApplyDataEntry(snapshot DataEntriesSnapshot) error {
 	blockID := a.info.BlockID()
 	for _, entry := range snapshot.DataEntries {
-		if err := a.accountsDataStor.appendEntry(snapshot.Address, entry, blockID); err != nil {
+		if err := a.stor.accountsDataStor.appendEntry(snapshot.Address, entry, blockID); err != nil {
 			return errors.Wrapf(err, "failed to add entry (%T) for address %q", entry, snapshot.Address)
 		}
 	}
@@ -209,5 +244,5 @@ func (a *blockSnapshotsApplier) ApplyLeaseState(snapshot LeaseStateSnapshot) err
 		CancelHeight:        snapshot.Status.CancelHeight,
 		CancelTransactionID: snapshot.Status.CancelTransactionID,
 	}
-	return a.leases.addLeasing(snapshot.LeaseID, l, a.info.BlockID())
+	return a.stor.leases.addLeasing(snapshot.LeaseID, l, a.info.BlockID())
 }
