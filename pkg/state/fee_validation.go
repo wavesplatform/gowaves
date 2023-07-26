5f6b4d9653642e825454795f7cb0943db00758b9
Nikolay Eskov <mr.eskov1@yandex.ru>
2023-07-26 15:44:22 +0300

    Use original estimated verifier complexity for accounts with verifier. (#1167)

--- pkg/state/fee_validation.go
+++ pkg/state/fee_validation.go
@@ -264,9 +264,10 @@ func scriptsCost(tx proto.Transaction, params *feeValidationParams) (*txCosts, e
 	// check complexity of script for free verifier if complexity <= 200
 	complexity := 0
 	if accountScripted && params.rideV5Activated {
-		treeEstimation, err := params.stor.scriptsComplexity.newestScriptComplexityByAddr(senderAddr, params.estimatorVersion)
-		if err != nil {
-			return nil, errors.Errorf("failed to get complexity by addr from store, %v", err)
+		// For account script we use original estimation
+		treeEstimation, scErr := params.stor.scriptsComplexity.newestOriginalScriptComplexityByAddr(senderWavesAddr)
+		if scErr != nil {
+			return nil, errors.Wrap(scErr, "failed to get complexity by addr from store")
 		}
 		complexity = treeEstimation.Verifier
 	}
