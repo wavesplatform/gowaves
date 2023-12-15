97c1ac08a06a78516456f579e8cea44eb872977d
irina-pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2023-12-15 19:20:48 +0300

    Issue API Smoke suite.

--- itests/issue_tx_api_internal_test.go
+++ itests/issue_tx_api_internal_test.go
@@ -1,6 +1,7 @@
 package itests
 
 import (
+	"math/rand"
 	"net/http"
 	"testing"
 
@@ -16,7 +17,35 @@ type IssueTxApiSuite struct {
 	f.BaseSuite
 }
 
+func issuePositiveAPIChecks(t *testing.T, tx utl.ConsideredTransaction,
+	td testdata.IssueTestData[testdata.ExpectedValuesPositive],
+	actualDiffBalanceInWaves utl.BalanceInWaves, actualDiffBalanceInAsset utl.BalanceInAsset, errMsg string) {
+	utl.StatusCodesCheck(t, http.StatusOK, http.StatusOK, tx, errMsg)
+	utl.TxInfoCheck(t, tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
+	utl.WavesDiffBalanceCheck(t, td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
+		actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
+	utl.AssetDiffBalanceCheck(t, td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
+		actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+}
+
+func issueNegativeAPIChecks(t *testing.T, tx utl.ConsideredTransaction,
+	td testdata.IssueTestData[testdata.ExpectedValuesNegative],
+	actualDiffBalanceInWaves utl.BalanceInWaves, actualDiffBalanceInAsset utl.BalanceInAsset, errMsg string) {
+	utl.StatusCodesCheck(t, http.StatusInternalServerError, http.StatusBadRequest, tx, errMsg)
+	utl.ErrorMessageCheck(t, td.Expected.ErrBrdCstGoMsg, td.Expected.ErrBrdCstScalaMsg,
+		tx.BrdCstErr.ErrorBrdCstGo, tx.BrdCstErr.ErrorBrdCstScala, errMsg)
+	utl.ErrorMessageCheck(t, td.Expected.ErrGoMsg, td.Expected.ErrScalaMsg,
+		tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
+	utl.WavesDiffBalanceCheck(t, td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
+		actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
+	utl.AssetDiffBalanceCheck(t, td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
+		actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+}
+
 func (suite *IssueTxApiSuite) Test_IssueTxApiPositive() {
+	if testing.Short() {
+		suite.T().Skip("skipping long positive Issue API Tx tests in short mode")
+	}
 	versions := issue_utilities.GetVersions(&suite.BaseSuite)
 	waitForTx := true
 	for _, v := range versions {
@@ -27,19 +56,16 @@ func (suite *IssueTxApiSuite) Test_IssueTxApiPositive() {
 				tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(
 					&suite.BaseSuite, td, v, waitForTx)
 				errMsg := caseName + "Broadcast Issue tx:" + tx.TxID.String()
-
-				utl.StatusCodesCheck(suite.T(), http.StatusOK, http.StatusOK, tx, errMsg)
-				utl.TxInfoCheck(suite.T(), tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
-				utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
-					actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
-				utl.AssetDiffBalanceCheck(suite.T(), td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
-					actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+				issuePositiveAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset, errMsg)
 			})
 		}
 	}
 }
 
 func (suite *IssueTxApiSuite) Test_IssueTxApiWithSameDataPositive() {
+	if testing.Short() {
+		suite.T().Skip("skipping long positive Issue API Tx tests in short mode")
+	}
 	versions := issue_utilities.GetVersions(&suite.BaseSuite)
 	waitForTx := true
 	for _, v := range versions {
@@ -51,13 +77,7 @@ func (suite *IssueTxApiSuite) Test_IssueTxApiWithSameDataPositive() {
 					tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(&suite.BaseSuite,
 						testdata.DataChangedTimestamp(&td), v, waitForTx)
 					errMsg := caseName + "Broadcast Issue tx:" + tx.TxID.String()
-
-					utl.StatusCodesCheck(suite.T(), http.StatusOK, http.StatusOK, tx, errMsg)
-					utl.TxInfoCheck(suite.T(), tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
-					utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
-						actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
-					utl.AssetDiffBalanceCheck(suite.T(), td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
-						actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+					issuePositiveAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset, errMsg)
 				}
 			})
 		}
@@ -65,6 +85,9 @@ func (suite *IssueTxApiSuite) Test_IssueTxApiWithSameDataPositive() {
 }
 
 func (suite *IssueTxApiSuite) Test_IssueTxApiNegative() {
+	if testing.Short() {
+		suite.T().Skip("skipping long negative Issue API Tx tests in short mode")
+	}
 	versions := issue_utilities.GetVersions(&suite.BaseSuite)
 	waitForTx := true
 	txIds := make(map[string]*crypto.Digest)
@@ -77,18 +100,44 @@ func (suite *IssueTxApiSuite) Test_IssueTxApiNegative() {
 					&suite.BaseSuite, td, v, !waitForTx)
 				txIds[name] = &tx.TxID
 				errMsg := caseName + "Broadcast Issue tx:" + tx.TxID.String()
-
-				utl.StatusCodesCheck(suite.T(), http.StatusInternalServerError, http.StatusBadRequest, tx, errMsg)
-				utl.ErrorMessageCheck(suite.T(), td.Expected.ErrBrdCstGoMsg, td.Expected.ErrBrdCstScalaMsg,
-					tx.BrdCstErr.ErrorBrdCstGo, tx.BrdCstErr.ErrorBrdCstScala, errMsg)
-				utl.ErrorMessageCheck(suite.T(), td.Expected.ErrGoMsg, td.Expected.ErrScalaMsg,
-					tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
-				utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
-					actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
-				utl.AssetDiffBalanceCheck(suite.T(), td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
-					actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+				issueNegativeAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset,
+					errMsg)
 			})
 		}
+	}
+	actualTxIds := utl.GetTxIdsInBlockchain(&suite.BaseSuite, txIds)
+	suite.Lenf(actualTxIds, 0, "IDs: %#v", actualTxIds)
+}
+
+func (suite *IssueTxApiSuite) Test_IssueTxApiSmokePositive() {
+	versions := issue_utilities.GetVersions(&suite.BaseSuite)
+	randV := versions[rand.Intn(len(versions))]
+	tdmatrix := utl.GetRandomValueFromMap(testdata.GetPositiveDataMatrix(&suite.BaseSuite))
+	for name, td := range tdmatrix {
+		caseName := utl.GetTestcaseNameWithVersion(name, randV)
+		suite.Run(caseName, func() {
+			tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(
+				&suite.BaseSuite, td, randV, true)
+			errMsg := caseName + "Broadcast Issue tx:" + tx.TxID.String()
+			issuePositiveAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset, errMsg)
+		})
+	}
+}
+
+func (suite *IssueTxApiSuite) Test_IssueTxApiSmokeNegative() {
+	versions := issue_utilities.GetVersions(&suite.BaseSuite)
+	randV := versions[rand.Intn(len(versions))]
+	txIds := make(map[string]*crypto.Digest)
+	tdmatrix := utl.GetRandomValueFromMap(testdata.GetNegativeDataMatrix(&suite.BaseSuite))
+	for name, td := range tdmatrix {
+		caseName := utl.GetTestcaseNameWithVersion(name, randV)
+		suite.Run(caseName, func() {
+			tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(
+				&suite.BaseSuite, td, randV, false)
+			txIds[name] = &tx.TxID
+			errMsg := caseName + "Broadcast Issue tx:" + tx.TxID.String()
+			issueNegativeAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset, errMsg)
+		})
 	}
 	actualTxIds := utl.GetTxIdsInBlockchain(&suite.BaseSuite, txIds)
 	suite.Lenf(actualTxIds, 0, "IDs: %#v", actualTxIds)
