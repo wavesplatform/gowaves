be168cb5c85c7379e0204ae8cc1fef85dcb7d534
irina-pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2023-12-15 16:33:22 +0300

    Issue Smart Asset API Smoke suite.

--- itests/issue_smart_asset_api_internal_test.go
+++ itests/issue_smart_asset_api_internal_test.go
@@ -1,6 +1,7 @@
 package itests
 
 import (
+	"math/rand"
 	"net/http"
 	"testing"
 
@@ -16,7 +17,37 @@ type IssueSmartAssetApiSuite struct {
 	f.BaseSuite
 }
 
+func issueSmartAssetPositiveAPIChecks(t *testing.T, tx utl.ConsideredTransaction,
+	td testdata.IssueTestData[testdata.ExpectedValuesPositive],
+	actualDiffBalanceInWaves utl.BalanceInWaves, actualDiffBalanceInAsset utl.BalanceInAsset,
+	actualScriptGo, actualScriptScala []byte, errMsg string) {
+	utl.StatusCodesCheck(t, http.StatusOK, http.StatusOK, tx, errMsg)
+	utl.TxInfoCheck(t, tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
+	utl.WavesDiffBalanceCheck(t, td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
+		actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
+	utl.AssetDiffBalanceCheck(t, td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
+		actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+	utl.AssetScriptCheck(t, td.Script, actualScriptGo, actualScriptScala, errMsg)
+}
+
+func issueSmartAssetNegativeAPIChecks(t *testing.T, tx utl.ConsideredTransaction,
+	td testdata.IssueTestData[testdata.ExpectedValuesNegative],
+	actualDiffBalanceInWaves utl.BalanceInWaves, actualDiffBalanceInAsset utl.BalanceInAsset, errMsg string) {
+	utl.StatusCodesCheck(t, http.StatusInternalServerError, http.StatusBadRequest, tx, errMsg)
+	utl.ErrorMessageCheck(t, td.Expected.ErrBrdCstGoMsg, td.Expected.ErrBrdCstScalaMsg,
+		tx.BrdCstErr.ErrorBrdCstGo, tx.BrdCstErr.ErrorBrdCstScala, errMsg)
+	utl.ErrorMessageCheck(t, td.Expected.ErrGoMsg, td.Expected.ErrScalaMsg,
+		tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
+	utl.WavesDiffBalanceCheck(t, td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
+		actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
+	utl.AssetDiffBalanceCheck(t, td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
+		actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+}
+
 func (suite *IssueSmartAssetApiSuite) Test_IssueSmartAssetApiPositive() {
+	if testing.Short() {
+		suite.T().Skip("skipping long positive Issue Smart Asset API Tx tests in short mode")
+	}
 	versions := issue_utilities.GetVersionsSmartAsset(&suite.BaseSuite)
 	waitForTx := true
 	for _, v := range versions {
@@ -26,24 +57,19 @@ func (suite *IssueSmartAssetApiSuite) Test_IssueSmartAssetApiPositive() {
 			suite.Run(caseName, func() {
 				tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(
 					&suite.BaseSuite, td, v, waitForTx)
-				errMsg := caseName + "Broadcast Issue smart asset tx:" + tx.TxID.String()
-
-				utl.StatusCodesCheck(suite.T(), http.StatusOK, http.StatusOK, tx, errMsg)
-				utl.TxInfoCheck(suite.T(), tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
-				utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
-					actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
-				utl.AssetDiffBalanceCheck(suite.T(), td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
-					actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
-
 				assetDetailsGo, assetDetailsScala := utl.GetAssetInfoGrpc(&suite.BaseSuite, tx.TxID)
-				utl.AssetScriptCheck(suite.T(), td.Script, assetDetailsGo.Script.ScriptBytes, assetDetailsScala.Script.ScriptBytes,
-					errMsg)
+				errMsg := caseName + "Broadcast Issue smart asset tx:" + tx.TxID.String()
+				issueSmartAssetPositiveAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset,
+					assetDetailsGo.Script.ScriptBytes, assetDetailsScala.Script.ScriptBytes, errMsg)
 			})
 		}
 	}
 }
 
 func (suite *IssueSmartAssetApiSuite) Test_IssueSmartAssetApiNegative() {
+	if testing.Short() {
+		suite.T().Skip("skipping long negative Issue Smart Asset API Tx tests in short mode")
+	}
 	versions := issue_utilities.GetVersionsSmartAsset(&suite.BaseSuite)
 	waitForTx := true
 	txIds := make(map[string]*crypto.Digest)
@@ -56,18 +82,47 @@ func (suite *IssueSmartAssetApiSuite) Test_IssueSmartAssetApiNegative() {
 					&suite.BaseSuite, td, v, !waitForTx)
 				txIds[name] = &tx.TxID
 				errMsg := caseName + "Broadcast Issue smart asset tx:" + tx.TxID.String()
-
-				utl.StatusCodesCheck(suite.T(), http.StatusInternalServerError, http.StatusBadRequest, tx, errMsg)
-				utl.ErrorMessageCheck(suite.T(), td.Expected.ErrBrdCstGoMsg, td.Expected.ErrBrdCstScalaMsg,
-					tx.BrdCstErr.ErrorBrdCstGo, tx.BrdCstErr.ErrorBrdCstScala, errMsg)
-				utl.ErrorMessageCheck(suite.T(), td.Expected.ErrGoMsg, td.Expected.ErrScalaMsg,
-					tx.WtErr.ErrWtGo, tx.WtErr.ErrWtScala, errMsg)
-				utl.WavesDiffBalanceCheck(suite.T(), td.Expected.WavesDiffBalance, actualDiffBalanceInWaves.BalanceInWavesGo,
-					actualDiffBalanceInWaves.BalanceInWavesScala, errMsg)
-				utl.AssetDiffBalanceCheck(suite.T(), td.Expected.AssetBalance, actualDiffBalanceInAsset.BalanceInAssetGo,
-					actualDiffBalanceInAsset.BalanceInAssetScala, errMsg)
+				issueSmartAssetNegativeAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset,
+					errMsg)
 			})
 		}
+	}
+	actualTxIds := utl.GetTxIdsInBlockchain(&suite.BaseSuite, txIds)
+	suite.Lenf(actualTxIds, 0, "IDs: %#v", actualTxIds)
+}
+
+func (suite *IssueSmartAssetApiSuite) Test_IssueSmartAssetApiSmokePositive() {
+	versions := issue_utilities.GetVersionsSmartAsset(&suite.BaseSuite)
+	randV := versions[rand.Intn(len(versions))]
+	tdmatrix := utl.GetRandomValueFromMap(testdata.GetPositiveAssetScriptData(&suite.BaseSuite))
+	for name, td := range tdmatrix {
+		caseName := utl.GetTestcaseNameWithVersion(name, randV)
+		suite.Run(caseName, func() {
+			tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(
+				&suite.BaseSuite, td, randV, true)
+			assetDetailsGo, assetDetailsScala := utl.GetAssetInfoGrpc(&suite.BaseSuite, tx.TxID)
+			errMsg := caseName + "Broadcast Issue smart asset tx:" + tx.TxID.String()
+			issueSmartAssetPositiveAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset,
+				assetDetailsGo.Script.ScriptBytes, assetDetailsScala.Script.ScriptBytes, errMsg)
+		})
+	}
+}
+
+func (suite *IssueSmartAssetApiSuite) Test_IssueSmartAssetApiSmokeNegative() {
+	versions := issue_utilities.GetVersionsSmartAsset(&suite.BaseSuite)
+	randV := versions[rand.Intn(len(versions))]
+	txIds := make(map[string]*crypto.Digest)
+	tdmatrix := utl.GetRandomValueFromMap(testdata.GetNegativeAssetScriptData(&suite.BaseSuite))
+	for name, td := range tdmatrix {
+		caseName := utl.GetTestcaseNameWithVersion(name, randV)
+		suite.Run(caseName, func() {
+			tx, actualDiffBalanceInWaves, actualDiffBalanceInAsset := issue_utilities.BroadcastIssueTxAndGetBalances(
+				&suite.BaseSuite, td, randV, false)
+			txIds[name] = &tx.TxID
+			errMsg := caseName + "Broadcast Issue smart asset tx:" + tx.TxID.String()
+			issueSmartAssetNegativeAPIChecks(suite.T(), tx, td, actualDiffBalanceInWaves, actualDiffBalanceInAsset,
+				errMsg)
+		})
 	}
 	actualTxIds := utl.GetTxIdsInBlockchain(&suite.BaseSuite, txIds)
 	suite.Lenf(actualTxIds, 0, "IDs: %#v", actualTxIds)
