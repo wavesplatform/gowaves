c6e3a2085e534aafdcaed6d16854b8ac503ef191
esuwu <sanya554455@gmail.com>
2024-01-14 23:30:50 -0600

    Initial test

--- pkg/state/state_hasher_test.go
+++ pkg/state/state_hasher_test.go
@@ -1,6 +1,8 @@
 package state
 
 import (
+	"github.com/opencontainers/go-digest"
+	"github.com/wavesplatform/gowaves/pkg/crypto"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -43,4 +45,57 @@ func TestPushOneBlock(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, h1, sh.stateHashAt(id1))
 	assert.Equal(t, sh.emptyHash, sh.stateHashAt(id2))
+}
+
+// TODO do the actual initial balances in appender
+
+func TestLegacyStateHashSupport(t *testing.T) {
+	initialBalances := newInitialBalancesInBlock()
+
+	snapshots1 := []proto.AtomicSnapshot {
+		&proto.WavesBalanceSnapshot{},
+	}
+
+	snapshots := []proto.AtomicSnapshot {
+			&proto.WavesBalanceSnapshot{Address:testAddr ,Balance: 1},
+			&proto.WavesBalanceSnapshot{Address: same ,Balance: 5},
+			&proto.WavesBalanceSnapshot{Address: ,Balance: 3},
+			&proto.WavesBalanceSnapshot{Address: same ,Balance: 5},
+
+			&proto.AssetBalanceSnapshot{
+				Address: proto.WavesAddress{},
+				AssetID: crypto.MustDigestFromBase58(assetStr),
+				Balance: 3,
+			},
+			&proto.AssetBalanceSnapshot{
+				Address: proto.WavesAddress{},
+				AssetID: crypto.MustDigestFromBase58(assetStr),
+				Balance: 0,
+			},
+			proto.AssetBalanceSnapshot{
+				Address: proto.WavesAddress{},
+				AssetID: crypto.MustDigestFromBase58(assetStr),
+				Balance: 3,
+			},
+
+			proto.LeaseBalanceSnapshot{
+				Address: proto.MustAddressFromString(testAddr),
+				LeaseIn: 10,
+				LeaseOut: 10,
+			},
+			proto.LeaseCancel{
+				Address: proto.WavesAddress{},
+				AssetID: crypto.Digest{},
+				Balance: 0,
+			},
+
+			proto.LeaseBalanceSnapshot{
+				Address: proto.WavesAddress{},
+				AssetID: crypto.Digest{},
+				Balance: 0,
+			},
+	}
+
+	initialBalances.addIfNotExists(txSnapshots.regular)
+
 }
