62ae65cd7d114825e9dfe8ad740cf51964306513
Nikolay Eskov <mr.eskov1@yandex.ru>
2024-02-20 23:38:08 +0300

    Fix 'TestCreateDiffLeaseWithSig'.

--- pkg/state/transaction_differ_test.go
+++ pkg/state/transaction_differ_test.go
@@ -885,15 +885,18 @@ func createLeaseWithSig(t *testing.T) *proto.LeaseWithSig {
 func TestCreateDiffLeaseWithSig(t *testing.T) {
 	checkerInfo := defaultCheckerInfo()
 	to := createDifferTestObjects(t, checkerInfo)
+	to.stor.addBlock(t, blockID0) // act as genesis block
+	to.stor.activateFeature(t, int16(settings.NG))
 
 	tx := createLeaseWithSig(t)
 	ch, err := to.td.createDiffLeaseWithSig(tx, defaultDifferInfo())
 	assert.NoError(t, err, "createDiffLeaseWithSig() failed")
 
+	minerFee := calculateCurrentBlockTxFee(tx.Fee, true) // NG is activated
 	correctDiff := txDiff{
 		testGlobal.senderInfo.wavesKey:    newBalanceDiff(-int64(tx.Fee), 0, int64(tx.Amount), false),
 		testGlobal.recipientInfo.wavesKey: newBalanceDiff(0, int64(tx.Amount), 0, false),
-		testGlobal.minerInfo.wavesKey:     newBalanceDiff(int64(tx.Fee), 0, 0, false),
+		testGlobal.minerInfo.wavesKey:     newBalanceDiff(int64(minerFee), 0, 0, false),
 	}
 	assert.Equal(t, correctDiff, ch.diff)
 	correctAddrs := map[proto.WavesAddress]struct{}{
