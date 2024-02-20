a16cd06d73d105958bf23aadb72a47a0d8372456
Nikolay Eskov <mr.eskov1@yandex.ru>
2024-02-20 23:29:43 +0300

    Fix 'TestCreateDiffReissueWithSig'.

--- pkg/state/transaction_differ_test.go
+++ pkg/state/transaction_differ_test.go
@@ -326,15 +326,17 @@ func createReissueWithSig(t *testing.T, feeUnits int) *proto.ReissueWithSig {
 func TestCreateDiffReissueWithSig(t *testing.T) {
 	checkerInfo := defaultCheckerInfo()
 	to := createDifferTestObjects(t, checkerInfo)
+	to.stor.addBlock(t, blockID0) // act as genesis block
+	to.stor.activateFeature(t, int16(settings.NG))
 
 	tx := createReissueWithSig(t, 1000)
 	ch, err := to.td.createDiffReissueWithSig(tx, defaultDifferInfo())
 	assert.NoError(t, err, "createDiffReissueWithSig() failed")
-
+	minerFee := calculateCurrentBlockTxFee(tx.Fee, true) // NG is activated
 	correctDiff := txDiff{
 		testGlobal.senderInfo.assetKeys[0]: newBalanceDiff(int64(tx.Quantity), 0, 0, false),
 		testGlobal.senderInfo.wavesKey:     newBalanceDiff(-int64(tx.Fee), 0, 0, false),
-		testGlobal.minerInfo.wavesKey:      newBalanceDiff(int64(tx.Fee), 0, 0, false),
+		testGlobal.minerInfo.wavesKey:      newBalanceDiff(int64(minerFee), 0, 0, false),
 	}
 	assert.Equal(t, correctDiff, ch.diff)
 	correctAddrs := map[proto.WavesAddress]struct{}{
