45523aa26ee8d78c9fae12d3b9661f4d7340e6e9
esuwu <sanya554455@gmail.com>
2025-01-13 12:14:22 +0300

    Formatted a line correctly

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -844,7 +844,8 @@ func runBlockchainUpdatesPlugin(
 		}
 	}()
 
-	return blockchaininfo.NewBlockchainUpdatesExtension(ctx, l2address, updatesChannel, requestsChannel, bUpdatesExtensionState), nil
+	return blockchaininfo.NewBlockchainUpdatesExtension(ctx, l2address, updatesChannel,
+		requestsChannel, bUpdatesExtensionState), nil
 }
 
 func FromArgs(scheme proto.Scheme, c *config) func(s *settings.NodeSettings) error {
