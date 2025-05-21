866ced03eab6ecdf05a5780669b06b76717d40bd
DrBlast <eugene.nichegovskiy@gmail.com>
2025-05-21 03:56:49 +0300

    docker-entrypoint.sh update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 set -e
 
-if [ $# -eq 0 ]; then
-  exec /app/node \
+exec /app/node \
     -cfg-path="$CONFIG_PATH" \
     -wallet-path="$WALLET_PATH" \
     -wallet-password="$WALLET_PASSWORD" \
     -state-path="$STATE_PATH" \
     -bind-address="$BIND_ADDR" \
     -api-address="$API_ADDR" \
-    -grpc-address="$GRPC_ADDR" \
     -build-extended-api \
     -serve-extended-api \
     -build-state-hashes \
     -enable-grpc-api
-else
-  exec /app/node "$@"
-fi
