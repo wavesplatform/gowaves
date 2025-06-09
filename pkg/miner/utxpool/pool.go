37744d5fd6ca44e14373f5cb5349e31d81df40e5
esuwu <sanya554455@gmail.com>
2025-06-09 16:22:51 +0300

    Sending utx metrics after tx is in the utx pool

--- pkg/miner/utxpool/pool.go
+++ pkg/miner/utxpool/pool.go
@@ -8,6 +8,7 @@ import (
 	"github.com/mr-tron/base58"
 	"github.com/pkg/errors"
 	"github.com/wavesplatform/gowaves/pkg/crypto"
+	"github.com/wavesplatform/gowaves/pkg/metrics"
 	"github.com/wavesplatform/gowaves/pkg/proto"
 	"github.com/wavesplatform/gowaves/pkg/settings"
 	"github.com/wavesplatform/gowaves/pkg/types"
@@ -74,7 +75,12 @@ func (a *UtxImpl) Add(t proto.Transaction) error {
 	}
 	a.mu.Lock()
 	defer a.mu.Unlock()
-	return a.addWithBytes(t, bts)
+	addErr := a.addWithBytes(t, bts)
+	if addErr != nil {
+		return addErr
+	}
+	metrics.Utx(a.Count())
+	return nil
 }
 
 func (a *UtxImpl) AddBytes(bts []byte) error {
