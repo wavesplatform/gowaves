be06f622014baace267ba48499ba825720ba43af
Nikolay Eskov <mr.eskov1@yandex.ru>
2025-08-05 21:26:12 +0300

    Improved logging params parsing.

--- pkg/logging/parameters.go
+++ pkg/logging/parameters.go
@@ -6,30 +6,56 @@ import (
 	"log/slog"
 )
 
+const (
+	DefaultLogLevelFlag   = "info"
+	DefaultLoggerTypeFlag = "pretty"
+)
+
+type ParametersFlags struct {
+	LogLevel   string
+	LoggerType string
+}
+
 type Parameters struct {
 	Level slog.Level
 	Type  LoggerType
 
-	flagLogLevel   string
-	flagLoggerType string
+	flag ParametersFlags
+}
+
+// ParametersFromFlags creates a Parameters instance from the provided ParametersFlags.
+// If flags is nil, it initializes with default values.
+// It returns an error if the parameters cannot be parsed.
+func ParametersFromFlags(flags *ParametersFlags) (Parameters, error) {
+	if flags == nil {
+		flags = &ParametersFlags{
+			LogLevel:   DefaultLogLevelFlag,
+			LoggerType: DefaultLoggerTypeFlag,
+		}
+	}
+	p := Parameters{flag: *flags}
+	if err := p.Parse(); err != nil {
+		return Parameters{}, fmt.Errorf("failed to parse logger parameters: %w", err)
+	}
+	return p, nil
 }
 
 // Initialize adds logging command line parameters to the global flag set.
 func (p *Parameters) Initialize() {
-	flag.StringVar(&p.flagLogLevel, "log-level", "info",
-		"Set the logging level. Supported values: debug, info, warn, error. Default: info.")
-	flag.StringVar(&p.flagLoggerType, "log-type", "pretty",
-		"Set the logger output format. Supported types: text, json, pretty. Default: pretty.")
+	flag.StringVar(&p.flag.LogLevel, "log-level", DefaultLogLevelFlag,
+		"Set the logging level. Supported values: debug, info, warn, error.")
+	flag.StringVar(&p.flag.LoggerType, "log-type", DefaultLoggerTypeFlag,
+		"Set the logger output format. Supported types: text, json, pretty.")
 }
 
 // Parse parses the command line parameters for logging.
 func (p *Parameters) Parse() error {
 	var err error
-	p.Level, err = p.parseLevel(p.flagLogLevel)
+	p.Level, err = p.parseLevel(p.flag.LogLevel)
 	if err != nil {
 		return fmt.Errorf("failed to parse logger parameters: %w", err)
 	}
-	p.Type, err = p.parseType(p.flagLoggerType)
+	p.Type, err = p.parseType(p.flag.LoggerType)
 	if err != nil {
 		return fmt.Errorf("failed to parse logger parameters: %w", err)
 	}
