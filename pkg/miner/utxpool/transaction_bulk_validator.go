3cfdfdf46b715afda42b8b0c0d04b21ae17f8d5f
Nikolay Eskov <mr.eskov1@yandex.ru>
2025-09-04 12:50:44 +0300

    Add more logs in 'bulkValidator.validate'.

--- pkg/miner/utxpool/transaction_bulk_validator.go
+++ pkg/miner/utxpool/transaction_bulk_validator.go
@@ -52,9 +52,14 @@ func (a bulkValidator) validate(ctx context.Context) []*types.TransactionWithByt
 	var transactions []*types.TransactionWithBytes
 	currentTimestamp := proto.NewTimestampFromTime(a.tm.Now())
 	lastKnownBlock := a.state.TopBlock()
+	dropped := 0
 	for checked := 0; ; checked++ { // just a counter for logging, checking until UTX is empty or context is done
 		if ctx.Err() != nil {
-			slog.Debug("Bulk validation interrupted", logging.Error(context.Cause(ctx)))
+			slog.Debug("Bulk validation interrupted", logging.Error(context.Cause(ctx)),
+				slog.Int("valid", len(transactions)),
+				slog.Int("checked", checked),
+				slog.Int("dropped", dropped),
+			)
 			return transactions
 		}
 		t := a.utx.Pop()
@@ -62,6 +67,7 @@ func (a bulkValidator) validate(ctx context.Context) []*types.TransactionWithByt
 			slog.Debug("UTX pool is empty, finished validating",
 				slog.Int("valid", len(transactions)),
 				slog.Int("checked", checked),
+				slog.Int("dropped", dropped),
 			)
 			break
 		}
@@ -69,13 +75,21 @@ func (a bulkValidator) validate(ctx context.Context) []*types.TransactionWithByt
 			_, err := validation.ValidateNextTx(t.T, currentTimestamp, lastKnownBlock.Timestamp, lastKnownBlock.Version, false)
 			return err
 		})
-		if stateerr.IsTxCommitmentError(err) {
-			slog.Error("Failed to unpack a transaction from UTX", logging.Error(err))
+		switch {
+		case stateerr.IsTxCommitmentError(err):
+			slog.Error("Failed to unpack a transaction from UTX",
+				logging.Error(err), logging.TxID(t.T, a.scheme),
+				slog.Int("valid", len(transactions)),
+				slog.Int("checked", checked),
+				slog.Int("dropped", dropped),
+			)
+			dropped++ // drop this tx
 			// This should not happen in practice.
 			// Reset state, return applied transactions to UTX.
 			for _, tx := range transactions {
 				utxErr := a.utx.AddWithBytesRaw(tx.T, tx.B)
 				if utxErr != nil {
+					dropped++ // drop this tx
 					slog.Error("Failed to return a transaction to UTX",
 						logging.Error(utxErr), logging.TxID(t.T, a.scheme),
 					)
@@ -84,11 +98,14 @@ func (a bulkValidator) validate(ctx context.Context) []*types.TransactionWithByt
 			slog.Debug("Valid transactions returned to UTX, resetting list and continuing",
 				slog.Int("returned", len(transactions)),
 				slog.Int("checked", checked),
+				slog.Int("dropped", dropped),
 			)
 			clear(transactions)             // Clear the slice to avoid memory leak
 			transactions = transactions[:0] // Reset the slice to empty
 			continue
-		} else if err == nil {
+		case err != nil: // invalid tx, just drop it
+			dropped++
+		default: // valid tx
 			transactions = append(transactions, t)
 		}
 	}
