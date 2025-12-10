b77abc1073fcb53fb4df68f776c47a8862c110ed
esuwu <sanya554455@gmail.com>
2025-12-10 15:03:04 +0100

    Added http client itests

--- pkg/client/generators.go
+++ pkg/client/generators.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"github.com/wavesplatform/gowaves/pkg/api"
 	"net/http"
 )
 
@@ -25,7 +26,7 @@ type GeneratorsAtResponse struct {
 
 // CommitmentGeneratorsAt returns the list of committed generators for the given height.
 func (a *Generators) CommitmentGeneratorsAt(ctx context.Context,
-	height uint64) (*GeneratorsAtResponse, *Response, error) {
+	height uint64) ([]api.GeneratorInfo, *Response, error) {
 	url, err := joinUrl(a.options.BaseUrl, fmt.Sprintf("/generators/at/%d", height))
 	if err != nil {
 		return nil, nil, err
@@ -34,8 +35,8 @@ func (a *Generators) CommitmentGeneratorsAt(ctx context.Context,
 	if err != nil {
 		return nil, nil, err
 	}
-	out := new(GeneratorsAtResponse)
-	resp, err := doHTTP(ctx, a.options, req, out)
+	var out []api.GeneratorInfo
+	resp, err := doHTTP(ctx, a.options, req, &out)
 	if err != nil {
 		return nil, resp, err
 	}
