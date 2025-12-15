d4f42cfb6a81c14bb4c55f7716d08a8ce376af74
Alexey Kiselev <alexey.kiselev@gmail.com>
2025-12-15 16:35:19 +0400

    Protobuf schemas updated and code regenerated.

--- pkg/grpc/generated/waves/block_vtproto.pb.go
+++ pkg/grpc/generated/waves/block_vtproto.pb.go
@@ -48,6 +48,16 @@ func (m *Block_Header_ChallengedHeader) MarshalToSizedBufferVTStrict(dAtA []byte
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if m.FinalizationVoting != nil {
+		size, err := m.FinalizationVoting.MarshalToSizedBufferVTStrict(dAtA[:i])
+		if err != nil {
+			return 0, err
+		}
+		i -= size
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(size))
+		i--
+		dAtA[i] = 0x4a
+	}
 	if len(m.HeaderSignature) > 0 {
 		i -= len(m.HeaderSignature)
 		copy(dAtA[i:], m.HeaderSignature)
@@ -634,6 +644,10 @@ func (m *Block_Header_ChallengedHeader) SizeVT() (n int) {
 	if l > 0 {
 		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
 	}
+	if m.FinalizationVoting != nil {
+		l = m.FinalizationVoting.SizeVT()
+		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
+	}
 	n += len(m.unknownFields)
 	return n
 }
@@ -1137,6 +1151,42 @@ func (m *Block_Header_ChallengedHeader) UnmarshalVT(dAtA []byte) error {
 			m.HeaderSignature = append(m.HeaderSignature[:0], dAtA[iNdEx:postIndex]...)
 			if m.HeaderSignature == nil {
 				m.HeaderSignature = []byte{}
+			}
+			iNdEx = postIndex
+		case 9:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field FinalizationVoting", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.FinalizationVoting == nil {
+				m.FinalizationVoting = &FinalizationVoting{}
+			}
+			if err := m.FinalizationVoting.UnmarshalVT(dAtA[iNdEx:postIndex]); err != nil {
+				return err
 			}
 			iNdEx = postIndex
 		default:
