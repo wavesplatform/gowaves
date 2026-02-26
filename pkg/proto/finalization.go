f6f2944c627a362d3455fca6f6bdcfed41940763
Alexey Kiselev <alexey.kiselev@gmail.com>
2026-02-26 18:18:51 +0400

    Added function to combine FinalizationVoting structures. Test added.

--- pkg/proto/finalization.go
+++ pkg/proto/finalization.go
@@ -3,6 +3,7 @@ package proto
 import (
 	"encoding/binary"
 	"log/slog"
+	"slices"
 
 	"github.com/ccoveille/go-safecast/v2"
 	"github.com/pkg/errors"
@@ -144,6 +145,21 @@ func (f *FinalizationVoting) ToProtobuf() (*g.FinalizationVoting, error) {
 		ConflictEndorsements:           conflictEndorsements,
 	}
 	return &finalizationVoting, nil
+}
+
+func CombineFinalizationVoting(voting1, voting2 *FinalizationVoting) *FinalizationVoting {
+	switch {
+	case voting1 == nil && voting2 == nil:
+		return nil
+	case voting1 == nil:
+		return voting2
+	case voting2 == nil:
+		return voting1
+	default:
+		res := *voting2
+		res.ConflictEndorsements = slices.Concat(voting1.ConflictEndorsements, voting2.ConflictEndorsements)
+		return &res
+	}
 }
 
 func CalculateLastFinalizedHeight(currentHeight Height) Height {
