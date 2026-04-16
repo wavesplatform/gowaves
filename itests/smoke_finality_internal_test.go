9c31ae12070cbd3de80f5fefe15b6dcf14da4c91
Alexey Kiselev <alexey.kiselev@gmail.com>
2026-04-16 14:26:48 +0400

    Correct BLS keys generation for Go and Scala miners. Updated genesis.json configuration to support go/scala/no -mining. Enabled log level check added before querying latest finalized block height.

--- itests/smoke_finality_internal_test.go
+++ itests/smoke_finality_internal_test.go
@@ -73,11 +73,13 @@ func (s *SmokeFinalitySuite) commitToGeneration(activationHeight uint64) uint64 
 	goErr, scalaErr := s.Clients.WaitForTransaction(*goTx.ID, txTimeout)
 	require.NoError(s.T(), goErr)
 	require.NoError(s.T(), scalaErr)
+	s.T().Logf("Go miner: Address: %s, BLS PK: %s", s.goMiner.Address, s.goMiner.BLSPublicKey.String())
 	s.T().Logf("Go commitment transaction ID: %s", *goTx.ID)
 
 	goErr, scalaErr = s.Clients.WaitForTransaction(*scalaTx.ID, txTimeout)
 	require.NoError(s.T(), goErr)
 	require.NoError(s.T(), scalaErr)
+	s.T().Logf("Scala miner: Address: %s, BLS PK: %s", s.scalaMiner.Address, s.scalaMiner.BLSPublicKey.String())
 	s.T().Logf("Scala commitment transaction ID: %s", *scalaTx.ID)
 	return gps
 }
